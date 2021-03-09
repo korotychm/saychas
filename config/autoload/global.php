ab3737196dc5b45632685d99df9b972817fa19f1
Alex <alex.kraskov@gmail.com>
2021-03-09 10:15:52 +0300

    Added Doctrine-Orm-Module

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -50,66 +50,64 @@ return [
             PDO::MYSQL_ATTR_INIT_COMMAND =>'SET NAMES \'UTF8\'',
         ],
     ],
-    /**
-    'doctrine' => [        
-        // migrations configuration
-//        'migrations_configuration' => [
-//            'orm_default' => [
-//                'directory' => 'data/Migrations',
-//                'name'      => 'Doctrine Database Migrations',
-//                'namespace' => 'Migrations',
-//                'table'     => 'migrations',
-//            ],
-//        ],
-        
-        'driver' => [
-            __NAMESPACE__ . '_driver' => [
-                'class' => AnnotationDriver::class,
-                'cache' => 'array',
-                'paths' => [__DIR__ . '/../../module/Application/src/Entity']
-            ],
-            'orm_default' => [
-                'drivers' => [
-                    __NAMESPACE__ . '\Application\Entity' => __NAMESPACE__ . '_driver'
-                ],
-            ],
-        ],
+    
+//    'doctrine' => [        
+//        // migrations configuration
+////        'migrations_configuration' => [
+////            'orm_default' => [
+////                'directory' => 'data/Migrations',
+////                'name'      => 'Doctrine Database Migrations',
+////                'namespace' => 'Migrations',
+////                'table'     => 'migrations',
+////            ],
+////        ],
+//        
 //        'driver' => [
-//            // defines an annotation driver with two paths, and names it `my_annotation_driver`
-//            'my_annotation_driver' => [
-//                'class' => \Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
+//            __NAMESPACE__ . '_driver' => [
+//                'class' => AnnotationDriver::class,
 //                'cache' => 'array',
-//                'paths' => [
-//                    'module/Entity',
-//                ],
+//                'paths' => [__DIR__ . '/../../module/Application/src/Entity']
 //            ],
-//
-//            // default metadata driver, aggregates all other drivers into a single one.
-//            // Override `orm_default` only if you know what you're doing
 //            'orm_default' => [
 //                'drivers' => [
-//                    // register `my_annotation_driver` for any entity under namespace `My\Namespace`
-//                    'My\Namespace' => 'my_annotation_driver',
+//                    __NAMESPACE__ . '\Application\Entity' => __NAMESPACE__ . '_driver'
 //                ],
 //            ],
 //        ],
-        'connection' => [
-            // default connection name
-            'orm_default' => [
-                'driverClass' => \Doctrine\DBAL\Driver\PDOMySql\Driver::class,
-                'params' => [
-                    'host'     => 'localhost',
-                    'port'     => '3306',
-                    'user'     => 'saychas_z',
-                    'password' => 'saychas_z',
-                    'dbname'   => 'saychas_z',
-                ],
-            ],
-        ],
-        'directory' => 'data/DoctrineORMModule/Proxy',
-    ],
-     * 
-     */
+////        'driver' => [
+////            // defines an annotation driver with two paths, and names it `my_annotation_driver`
+////            'my_annotation_driver' => [
+////                'class' => \Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
+////                'cache' => 'array',
+////                'paths' => [
+////                    'module/Entity',
+////                ],
+////            ],
+////
+////            // default metadata driver, aggregates all other drivers into a single one.
+////            // Override `orm_default` only if you know what you're doing
+////            'orm_default' => [
+////                'drivers' => [
+////                    // register `my_annotation_driver` for any entity under namespace `My\Namespace`
+////                    'My\Namespace' => 'my_annotation_driver',
+////                ],
+////            ],
+////        ],
+//        'connection' => [
+//            // default connection name
+//            'orm_default' => [
+//                'driverClass' => \Doctrine\DBAL\Driver\PDOMySql\Driver::class,
+//                'params' => [
+//                    'host'     => 'localhost',
+//                    'port'     => '3306',
+//                    'user'     => 'saychas_z',
+//                    'password' => 'saychas_z',
+//                    'dbname'   => 'saychas_z',
+//                ],
+//            ],
+//        ],
+//        'directory' => 'data/DoctrineORMModule/Proxy',
+//    ],
         
 ];
 
