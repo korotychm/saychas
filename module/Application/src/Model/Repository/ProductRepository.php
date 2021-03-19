b2a699b26247eee08090f0adc786efe7fec1c6f9
Alex Kraskov <alex.kraskov@gmail.com>
2021-03-19 15:41:57 +0300

    fix

--- module/Application/src/Model/Repository/ProductRepository.php
+++ module/Application/src/Model/Repository/ProductRepository.php
@@ -170,7 +170,7 @@ class ProductRepository implements ProductRepositoryInterface
         $driver = $this->db->getDriver();
         foreach($result as $row) {
             $sql = sprintf("replace INTO `product`( `id`, `provider_id`, `category_id`, `title`, `description`, `vendor_code`) VALUES ( '%s', '%s', %u, '%s', '%s', '%s' )",
-                    $row['id'], $row['provider_id'], $row['category_id'], $row['title'], $row['description'], $row['vendor_code'] );
+                    $row['id'], $row['provider_id'], $row['category_id'], $row['title'], $row['description'], addslashes($row['vendor_code']) );
             //mysql_real_escape_string()
             try {
                 $selectString = $sql->buildSqlString($sql);
