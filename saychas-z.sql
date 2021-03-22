947786a8574c649ce60a7fa1f6b1eabc66c2063c
Alex Kraskov <alex.kraskov@gmail.com>
2021-03-22 09:56:57 +0300

    vendor_code length increased up to 100 symbols

--- saychas-z.sql
+++ saychas-z.sql
@@ -553,7 +553,7 @@ CREATE TABLE `product` (
   `category_id` VARCHAR(9) NOT NULL,
   `title` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `vendor_code` varchar(22) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+  `vendor_code` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
