22900e101da5246d3fabba705dd53f3b41fe16f3
Alex Kraskov <alex.kraskov@gmail.com>
2021-04-01 05:56:20 +0300

    fix

--- proc8.sql
+++ proc8.sql
@@ -1,12 +1,12 @@
 with recursive cte (id, name, parent_id) as (
   select     id,
-             name,
+             title,
              parent_id
   from       category
   where      parent_id = '000000003'
   union all
   select     p.id,
-             p.name,
+             p.title,
              p.parent_id
   from       category p
   inner join cte
