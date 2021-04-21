07f3036989c99f66310eba1d6e9f01d550d012b0
Alex <admin@plus3w.com>
2021-04-21 07:15:12 +0300

    changed

--- module/Application/src/Controller/MyTestController.php
+++ module/Application/src/Controller/MyTestController.php
@@ -1,8 +1,6 @@
 <?php
 /**
- * @see       https://github.com/laminas/laminas-mvc-skeleton for the canonical source repository
- * @copyright https://github.com/laminas/laminas-mvc-skeleton/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-mvc-skeleton/blob/master/LICENSE.md New BSD License
+ * changed
  */
 
 declare(strict_types=1);
