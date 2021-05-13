5b2013582daff7f723aca85361859bc2847f1e24
Alex Kraskov <alex.kraskov@gmail.com>
2021-05-13 15:03:08 +0300

    fix

--- module/Application/src/Model/Repository/ProductRepository.php
+++ module/Application/src/Model/Repository/ProductRepository.php
@@ -538,12 +538,17 @@ End of number 1 */
             }
             
             $arr1 = $this->updateCharacteristicsValueList($product);
-            
+
+//            echo '<pre>';
 //            print_r($arr1);
+//            echo '</pre>';
 //            exit;
 
             $sql = sprintf("replace INTO `product`( `id`, `provider_id`, `category_id`, `title`, `description`, `vendor_code`, `param_value_list`, `param_variable_list`, `brand_id` ) VALUES ( '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s' )",
                     $product->id, $product->provider_id, $product->category_id, $product->title, $product->description, $product->vendor_code, $arr1['value_list'], $arr1['var_list'], $product->brand_id);
+            
+            print_r($sql);
+            continue;
             try {
                 $query = $this->db->query($sql);
                 $query->execute();
