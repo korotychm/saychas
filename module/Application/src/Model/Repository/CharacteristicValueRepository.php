ac4dfc9e04bf382a05646636fab1af0d26658de4
Alex Kraskov <alex.kraskov@gmail.com>
2021-06-21 09:50:32 +0300

    fix

--- module/Application/src/Model/Repository/CharacteristicValueRepository.php
+++ module/Application/src/Model/Repository/CharacteristicValueRepository.php
@@ -62,8 +62,9 @@ class CharacteristicValueRepository extends Repository implements Characteristic
             return ['result' => false, 'description' => $e->getMessage(), 'statusCode' => 400];
         }
 
-        $this->mclient->saychas->characteristic_value->drop();
-        $this->mclient->saychas->characteristic_value->insertMany($result['data']);
+        $tableName = $this->tableName;
+        $this->mclient->saychas->$tableName->drop();
+        $this->mclient->saychas->$tableName->insertMany($result['data']);
         
         if ((bool) $result['truncate']) {
             $this->db->query("truncate table {$this->tableName}")->execute();
