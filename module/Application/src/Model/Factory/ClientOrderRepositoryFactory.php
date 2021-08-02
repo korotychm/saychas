e3f74f5e2bf0973b169b24053e99524131dedd97
Alex Kraskov <alex.kraskov@gmail.com>
2021-08-02 12:30:38 +0300

    ClientOrder added

---
+++ module/Application/src/Model/Factory/ClientOrderRepositoryFactory.php
@@ -0,0 +1,54 @@
+<?php
+
+// Application\src\Model\Repository\Factory\ClientOrderRepositoryFactory.php
+
+namespace Application\Model\Factory;
+
+use Interop\Container\ContainerInterface;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Application\Model\Entity\ClientOrder;
+use Application\Model\Repository\ClientOrderRepository;
+use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Hydrator\ClassMethodsHydrator;
+
+/**
+ * Description of ClientOrderRepositoryFactory
+ *
+ * @author alex
+ */
+class ClientOrderRepositoryFactory implements FactoryInterface
+{
+
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        if ($requestedName instanceof ClientOrderRepository) {
+            throw new Exception("not instanceof ClientOrderRepository");
+        }
+        
+        $adapter = $container->get(AdapterInterface::class);
+        
+        $hydrator = new ClassMethodsHydrator();
+        
+        $composite = new \Laminas\Hydrator\Filter\FilterComposite();
+        $composite->addFilter(
+                'excludeid',
+                new \Laminas\Hydrator\Filter\MethodMatchFilter('getId'),
+                \Laminas\Hydrator\Filter\FilterComposite::CONDITION_AND
+        );
+//        $composite->addFilter(
+//                'excludechildroles',
+//                new \Laminas\Hydrator\Filter\MethodMatchFilter('getChildRoles'),
+//                \Laminas\Hydrator\Filter\FilterComposite::CONDITION_AND
+//        );
+        $hydrator->addFilter('excludes', $composite, \Laminas\Hydrator\Filter\FilterComposite::CONDITION_AND);
+            
+        $prototype = new ClientOrder;
+
+        return new ClientOrderRepository(
+                $adapter,
+                $hydrator,
+                $prototype
+        );
+    }
+
+}
