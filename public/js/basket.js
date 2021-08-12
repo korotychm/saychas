7e0e1fba2c1f0607c4c0736c05a26275e2defbb6
Коротыч Михаил <m.korotich@saychas.office>
2021-08-12 11:07:23 +0300

    tolocale fix

--- public/js/basket.js
+++ public/js/basket.js
@@ -196,7 +196,7 @@ function calculateBasketPayCard ()
                       if (product.oldprice){
                         //вывод измененной цены
                         productHtml += '<div>';
-                          productHtml += ('<div class="changed-products__from">' + product.oldprice.toLocaleString() + ' ₽</div>');
+                          productHtml += ('<div class="changed-products__from">' + parseInt(product.oldprice).toLocaleString() + ' ₽</div>');
                           productHtml += ('<div class="changed-products__to">' + product.price.toLocaleString() +' ₽</div>');
                         productHtml += '</div>';
                       }
