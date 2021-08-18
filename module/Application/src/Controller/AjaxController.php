7c4441b2bb81a37ff1cca41d94cac2d99ea1bb5d
Plusweb <plusw.ru@gmail.com>
2021-08-18 16:26:43 +0300

    the waiting order status service for basket is ready

--- module/Application/src/Controller/AjaxController.php
+++ module/Application/src/Controller/AjaxController.php
@@ -179,6 +179,7 @@ class AjaxController extends AbstractActionController
         $return['userId'] = $userId = $container->userIdentity;
         if(!empty($order = ClientOrder::find(["order_id" => $orderId ]))){
             $return ['order_status'] = $order->getStatus();
+            //$return ['order_status'] = 1;  //test mode switch
             $deliveryinfo  = $order->getDeliveryInfo();
             $return['delivery_info'] = (!empty($deliveryinfo))?Json::decode($deliveryinfo, Json::TYPE_ARRAY):[];
         }   
