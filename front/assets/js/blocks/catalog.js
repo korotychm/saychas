59b6732ede62ab446c1ff90a02298423eec1181c
Коротыч Михаил <m.korotich@saychas.office>
2021-08-24 15:21:08 +0300

    test filters

--- front/assets/js/blocks/catalog.js
+++ front/assets/js/blocks/catalog.js
@@ -3,6 +3,28 @@ $(document).ready(function(){
   $('.select').niceSelect();
 });
 
+function getCategoryFilters(categoryId){
+      $.ajax({
+            beforeSend : function (){
+                },
+            url: "/ajax-get-category-filters",
+            type: 'POST',
+            cache: false,
+            data: {"categoryId": categoryId},
+            success: function (data) {
+              showServicePopupWindow("Фильтры дла каталога",JSON.stringify(data));
+              return false;
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+             if (xhr.status !== 0) {
+                    showAjaxErrorPopupWindow (xhr.status, thrownError);
+                }
+                return false;
+            }
+        });
+    return false;
+}
+
 function sendfilterform() {
     //alert("!!!!");
     var dataString = $("#filtrform").serialize();
@@ -58,6 +80,8 @@ $(document).ready(function () {
     $("#testProductButton").click(function(){
         sendfilterformAndGetJson();
     });
+
+    getCategoryFilters($("#testFiltersCategotyId").val());
 
     $("#filtrform").on("change", " input", function () {
         sendfilterform();
