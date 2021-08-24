724a1d8b84b45fb88fae2ce1f66f5509e921ba4f
Коротыч Михаил <m.korotich@saychas.office>
2021-08-24 18:59:41 +0300

    test

--- front/assets/js/blocks/catalog.js
+++ front/assets/js/blocks/catalog.js
@@ -1,72 +1,3 @@
-function getCategoryFilters(categoryId){
-      $.ajax({
-            beforeSend : function (){
-                },
-            url: "/ajax-get-category-filters",
-            type: 'POST',
-            cache: false,
-            data: {"categoryId": categoryId},
-            success: function (data) {
-              var filters = data;
-              for (let filter of filters.filters) {
-                if (filter.type == 2){
-                  let min = filter.options.reduce(function(prev, curr) {
-                    return +prev.value < +curr.value ? prev : curr;
-                  }).value;
-                  let max = filter.options.reduce(function(prev, curr) {
-                    return +prev.value > +curr.value ? prev : curr;
-                  }).value;
-
-                  let diff = max - min;
-                  let float = false;
-
-                  for (option of filter.options){
-                    if (isFloat(+option.value)){
-                      float = true;
-                      break;
-                    }
-                  }
-
-                  let step = 1;
-                  if (diff > 10000) {
-                    step = 100;
-                  }
-                  else if (diff > 1000) {
-                    step = 10;
-                  }
-                  else if (diff < 10 && float) {
-                    step = 0.1;
-                  }
-
-                  if (!float && diff % step != 0){
-                    max = +max + step;
-                  }
-
-                  filter.min = min;
-                  filter.max = max;
-                  filter.step = step;
-
-                }
-              }
-              var filter = new Vue({
-                el: '#catalogfilter',
-                data: filters
-              });
-              //Ranges
-              $('.range').each(function(){
-                setRange($(this));
-              });
-            },
-            error: function (xhr, ajaxOptions, thrownError) {
-             if (xhr.status !== 0) {
-                    showAjaxErrorPopupWindow (xhr.status, thrownError);
-                }
-                return false;
-            }
-        });
-}
-
-
 $(document).ready(function(){
 
   if ($('#catalogfilter').length){
@@ -142,29 +73,6 @@ $(document).ready(function(){
   }
 
 });
-
-function sendfilterform() {
-    //alert("!!!!");
-    var dataString = $("#filter-form").serialize();
-    $.ajax({
-        beforeSend: function () {
-            $("#overload").stop().show();
-        },
-        url: "/ajax-fltr",
-        type: 'POST',
-        cache: false,
-        data: dataString,
-        success: function (data) {
-            $("#tovar-list").html(data);
-            sendfilterformAndGetJson();
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            if (xhr.status != 0) {
-                showAjaxErrorPopupWindow (xhr.status, thrownError);
-            }
-        }
-    });
-}
 
 function sendfilterformAndGetJson() {
     //alert("!!!!");
