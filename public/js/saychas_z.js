45fc8f0821a108ffe4567d9b94c9d96a37891a07
Коротыч Михаил <m.korotich@saychas.office>
2021-08-24 10:37:20 +0300

    replace modal scripts

--- public/js/saychas_z.js
+++ public/js/saychas_z.js
@@ -1,26 +1,3 @@
-
-function showAjaxErrorPopupWindow (status, error){
-     showServicePopupWindow(
-                            "Ошибка " + status,
-                            "Ошибка соединения, попробуйте повторить попытку позже." + "\r\n " + status + " " + error
-                            );
-
-}
-
-
-function showServicePopupWindow(title, body, footer = "", noclose = false)
-{
-    $("#ServiceModalWindow .popup__close").removeClass('disabled');
-    $("#ServiceModalWindow .popup__heading").html(title);
-    $("#ServiceModalWindow #ServiceModalWraper").html(body);
-    $("#ServiceModalWindow #ServiceModalWraper").append(footer);
-    if (noclose) {
-        $("#ServiceModalWindow .popup__close").addClass('disabled');
-    }
-    $("#ServiceModalWindow").fadeIn();
-}
-
-
 $(document).ready(function () {
 
     showBasket(0);
