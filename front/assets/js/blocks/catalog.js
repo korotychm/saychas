f86b28e8edc7d3f3c09869c2301e52022e3558b2
Коротыч Михаил <m.korotich@saychas.office>
2021-08-25 15:08:42 +0300

    test

--- front/assets/js/blocks/catalog.js
+++ front/assets/js/blocks/catalog.js
@@ -111,7 +111,7 @@ $(document).on('change','.range input[type="range"]',function(){
   } else {
     hidden.val(minVal + ';' + maxVal);
   }
-  $('#filter-button').trigger('click');
+  //$('#filter-button').trigger('click');
 });
 
 $(document).on('change','.tooltip-from',function(){
