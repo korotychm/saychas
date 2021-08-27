d9d78d600409f6661b1a9d1a0c1b108dc3de104c
Коротыч Михаил <m.korotich@saychas.office>
2021-08-27 13:26:35 +0300

    test

--- front/assets/js/common/common.js
+++ front/assets/js/common/common.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     $(".overcover").delay(500).fadeOut("slow");
     $(".phoneinput").mask("+7 (999) 999-99-99");
 
+    $('.select').niceSelect();
+
     window.onbeforeunload = function () {
         $(".overcover").stop().fadeIn();
     };
@@ -11,8 +13,4 @@ $(document).ready(function () {
 
 $(document).on('click', '.popup__close', function () {
     $(this).parent().parent().fadeOut();
-});
-
-$(document).ready(function(){
-  $('.select').niceSelect();
 });
