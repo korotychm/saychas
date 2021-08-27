d7d2861fad7a225c44cd49191eb760b42fdff7fc
Plusweb <plusw.ru@gmail.com>
2021-08-27 08:52:58 +0300

    fix

--- module/Application/src/Controller/AcquiringController.php
+++ module/Application/src/Controller/AcquiringController.php
@@ -229,11 +229,11 @@ class AcquiringController extends AbstractActionController
 			HTTP_CONNECTION: ". $_SERVER["HTTP_CONNECTION"]. "
 			HTTP_USER_AGENT: ". $_SERVER["HTTP_USER_AGENT"]. "
 			HTTP_ACCEPT: ". $_SERVER["HTTP_ACCEPT"]."
-			REQUES:".print_r($_REQUEST, true);
+			REQUEST:".print_r($_REQUEST, true);
 
             //$message = print_r($_REQUEST, true);
             mail("d.sizov@saychas.ru", "tinkoff.log", $message);
-            mail("user@localhost", "tinkoff.log", $message);
+            //mail("user@localhost", "tinkoff.log", $message);
 
             $response = new Response();
             $response->setStatusCode(Response::STATUS_CODE_200);
