83911fac84b1846947669d376e5427b567572c55
Plusweb <plusw.ru@gmail.com>
2021-08-30 16:55:47 +0300

    fix

--- module/Application/src/Controller/AcquiringController.php
+++ module/Application/src/Controller/AcquiringController.php
@@ -253,13 +253,13 @@ class AcquiringController extends AbstractActionController
                     $order->setPaymentInfo($jsonData);
                     $order->persist(["order_id" => $postData["OrderId"]]);
             }        
-        /*    if (!empty($postData["CardId"]) and !empty($clientOrder = ClientOrder::find(["order_id" => $postData["OrderId"]]))){
+       /**/if (!empty($postData["CardId"]) and !empty($clientOrder = ClientOrder::find(["order_id" => $postData["OrderId"]]))){
                    
                        $postData['user']=$userId = $clientOrder->getUserId();
                        $userPaycard = UserPaycard::findFirstOrDefault(['card_id' => $postData["CardId"], "user_id" => $userId]);
                        $userPaycard->setUserId($userId)->setCardId($postData["CardId"])->setPan($postData["Pan"])->setTime(time());
                        $userPaycard->persist(['card_id' => $postData["CardId"], "user_id" => $userId]);    
-            }*/
+            } /**/
            $postData['answer_1с']=$this->externalCommunication->sendOrderPaymentInfo($postData);
         }
         return new JsonModel($postData);
