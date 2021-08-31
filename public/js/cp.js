705c4a3fe4e36e250b5517a8a41544c8f74af62d
Коротыч Михаил <m.korotich@saychas.office>
2021-08-31 12:07:01 +0300

    test router

--- public/js/cp.js
+++ public/js/cp.js
@@ -1,7 +1,7 @@
 
 const Analytics = { template: '<header class="header"><h1 class="header__heading"><span>Аналитика</span></h1><div class="header__user"><div class="header__user-name"><h2>Сергей Заказчиков</h2><p>Администратор</p></div><div class="header__user-avatar">С</div></div></header>' }
 
-const Products = { template: '<header class="header"><h1 class="header__heading"><span>Мои товары</span></h1><div class="header__user"><div class="header__user-name"><h2>Сергей Заказчиков</h2><p>Администратор</p></div><div class="header__user-avatar">С</div></div></header>' }
+const Products = { template: '<header class="header"><h1 class="header__heading"><span>{{ test }}</span></h1><div class="header__user"><div class="header__user-name"><h2>Сергей Заказчиков</h2><p>Администратор</p></div><div class="header__user-avatar">С</div></div></header>' }
 
 const routes = [
   {
@@ -21,8 +21,11 @@ const router = new VueRouter({
   mode: 'history'
 })
 
-const app = new Vue({
+const cp = new Vue({
   router,
+  data: {
+    test: 'test vue'
+  },
   mounted: function(){
     router.replace('/control-panel/products');
   }
