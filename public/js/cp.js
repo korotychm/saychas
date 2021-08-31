56596a991aa9a10a722efac289a36729d9ee98ca
Коротыч Михаил <m.korotich@saychas.office>
2021-08-31 12:40:35 +0300

    test router

--- public/js/cp.js
+++ public/js/cp.js
@@ -1,14 +1,13 @@
 
 const Analytics = { template: '<header class="header"><h1 class="header__heading"><span>Аналитика</span></h1><div class="header__user"><div class="header__user-name"><h2>Сергей Заказчиков</h2><p>Администратор</p></div><div class="header__user-avatar">С</div></div></header>' }
-
-Vue.component('Products', {
+const Products = {
   data: function () {
     return {
       test: 'test vue'
     }
   },
-  template: '<header class="header"><h1 class="header__heading"><span>{{ test }}</span></h1><div class="header__user"><div class="header__user-name"><h2>Сергей Заказчиков</h2><p>Администратор</p></div><div class="header__user-avatar">С</div></div></header>'
-})
+  template: '<header class="header"><h1 class="header__heading"><span>Аналитика</span></h1><div class="header__user"><div class="header__user-name"><h2>Сергей Заказчиков</h2><p>Администратор</p></div><div class="header__user-avatar">С</div></div></header>'
+}
 
 const routes = [
   {
