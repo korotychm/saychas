945207ab48cb32723f416377f3589315ef4f4c38
Коротыч Михаил <m.korotich@saychas.office>
2021-08-31 11:29:12 +0300

    test router

--- public/js/cp.js
+++ public/js/cp.js
@@ -2,7 +2,7 @@
 const Foo = { template: '<div>foo</div>' }
 
 const routes = [
-  { path: '/foo', component: Foo }
+  { path: '/control-panel/foo', component: Foo }
 ]
 
 const router = new VueRouter({
@@ -11,5 +11,8 @@ const router = new VueRouter({
 })
 
 const app = new Vue({
-  router
+  router,
+  mounted: function(){
+    router.replace('/control-panel/foo');
+  }
 }).$mount('#cp')
