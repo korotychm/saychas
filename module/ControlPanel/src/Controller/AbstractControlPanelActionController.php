bd8057e0f89239d5bc692a088e091ff270b7fadf
Alex Kraskov <alex.kraskov@gmail.com>
2021-09-03 16:33:19 +0300

    AbstractControlPanelActionController added

---
+++ module/ControlPanel/src/Controller/AbstractControlPanelActionController.php
@@ -0,0 +1,33 @@
+<?php
+
+// ControlPanel/src/Controller/AbstractControlPanelActionController.php
+
+/**
+ * @author alex;
+ * @copyright (c) 2021, Alex
+ */
+namespace ControlPanel\Controller;
+
+use Laminas\Mvc\Controller\AbstractActionController;
+/**
+ * Basic action controller
+ */
+abstract class AbstractControlPanelActionController extends AbstractActionController
+{
+    
+    protected function getManagerInfo($routeMatch)
+    {
+        $routeName = $routeMatch->getMatchedRouteName();
+        
+        list($leftName, $rightName) = explode('/', $routeName);
+
+        //$params = $routeMatch->getParams();
+
+        $config = $this->container->get('Config');
+        
+        $managerName = $config['router']['routes'][$leftName]['child_routes'][$rightName]['options']['repository'];
+
+        return ['manager' => $this->container->get($managerName), 'manager_name' => $managerName];
+    }
+    
+}
