5855276da05ce2242ccbe3d4a24ca2f05edaccaf
Коротыч Михаил <m.korotich@saychas.office>
2021-09-09 17:44:37 +0300

    colors

--- public/js/cp.js
+++ public/js/cp.js
@@ -304,19 +304,6 @@ const ProductEdit = {
                                     <span class="color-checkbox__check-color" :style="{'backgroundColor' : color.value}"></span>
                                   </span>
                                 </label>
-                                <div class="search-select__suggestions">
-                                    <div v-if="!brandSearch" class="search-select__empty">Начните вводить название бренда для поиска</div>
-                                    <div v-if="(brandSearch && !filteredBrands.length)" class="search-select__empty">Ничего не найдено</div>
-                                    <div v-if="(brandSearch && filteredBrands.length)">
-                                      <label v-for="brand in filteredBrands">
-                                        <input type="radio" name="suggest" :checked="(brand.id == selectedBrandId)" />
-                                        <span class="search-select__suggestion" @click="selectBrand(brand.id, brand.title)">
-                                          <span>{{ brand.title }}</span>
-                                        </span>
-                                      </label>
-                                    </div>
-                                </div>
-
                             </div>
                       </div>
                   </div>
