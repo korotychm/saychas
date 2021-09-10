93e0bbed0ad77f1c629ee5925bcece9cf6cb578d
Alex Kraskov <alex.kraskov@gmail.com>
2021-09-10 16:09:43 +0300

    fix

--- module/Application/src/Model/Repository/StoreRepository.php
+++ module/Application/src/Model/Repository/StoreRepository.php
@@ -91,30 +91,30 @@ class StoreRepository extends Repository implements StoreRepositoryInterface
      *
      * @param json $content
      */
-    public function replace($content)
-    {
-        try {
-            $result = Json::decode($content, \Laminas\Json\Json::TYPE_ARRAY);
-        } catch (\Laminas\Json\Exception\RuntimeException $e) {
-            return ['result' => false, 'description' => $e->getMessage(), 'statusCode' => 400];
-        }
-
-        if ((bool) $result['truncate']) {
-            $this->db->query("truncate table {$this->tableName}")->execute();
-        }
-
-        foreach ($result['data'] as $row) {
-            $sql = sprintf("replace INTO `store`( `id`, `provider_id`, `title`, `description`, `address`, `geox`, `geoy`, `icon`) VALUES ( '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s' )",
-                    $row['id'], $row['provider_id'], $row['title'], $row['description'], $row['address'], $row['geox'], $row['geoy'], $row['icon']);
-
-            try {
-                $query = $this->db->query($sql);
-                $query->execute();
-            } catch (InvalidQueryException $e) {
-                return ['result' => false, 'description' => "error executing $sql", 'statusCode' => 418];
-            }
-        }
-        return ['result' => true, 'description' => '', 'statusCode' => 200];
-    }
+//    public function replace($content)
+//    {
+//        try {
+//            $result = Json::decode($content, \Laminas\Json\Json::TYPE_ARRAY);
+//        } catch (\Laminas\Json\Exception\RuntimeException $e) {
+//            return ['result' => false, 'description' => $e->getMessage(), 'statusCode' => 400];
+//        }
+//
+//        if ((bool) $result['truncate']) {
+//            $this->db->query("truncate table {$this->tableName}")->execute();
+//        }
+//
+//        foreach ($result['data'] as $row) {
+//            $sql = sprintf("replace INTO `store`( `id`, `provider_id`, `title`, `description`, `address`, `geox`, `geoy`, `icon`) VALUES ( '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s' )",
+//                    $row['id'], $row['provider_id'], $row['title'], $row['description'], $row['address'], $row['geox'], $row['geoy'], $row['icon']);
+//
+//            try {
+//                $query = $this->db->query($sql);
+//                $query->execute();
+//            } catch (InvalidQueryException $e) {
+//                return ['result' => false, 'description' => "error executing $sql", 'statusCode' => 418];
+//            }
+//        }
+//        return ['result' => true, 'description' => '', 'statusCode' => 200];
+//    }
 
 }
