709dafbc23600e8561068585e390877070e0a350
Alex Kraskov <alex.kraskov@gmail.com>
2021-09-10 16:04:58 +0300

    fix

--- module/Application/src/Model/Repository/StoreRepository.php
+++ module/Application/src/Model/Repository/StoreRepository.php
@@ -104,7 +104,7 @@ class StoreRepository extends Repository implements StoreRepositoryInterface
         }
 
         foreach ($result['data'] as $row) {
-            $sql = sprintf("replace INTO `store`( `id`, `provider_id`, `title`, `description`, `address`, `geox`, `geoy`, `icon`, `contact_phone`, `contact_person`) VALUES ( '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %u, '%s' )",
+            $sql = sprintf("replace INTO `store`( `id`, `provider_id`, `title`, `description`, `address`, `geox`, `geoy`, `icon`, `contact_phone`, `contact_person`) VALUES ( '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%u', '%s' )",
                     $row['id'], $row['provider_id'], $row['title'], $row['description'], $row['address'], $row['geox'], $row['geoy'], $row['icon'], $row['contact_phone'], $row['contact_person']);
 
             try {
