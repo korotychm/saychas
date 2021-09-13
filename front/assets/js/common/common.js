6da085b92c56f43c3707a606cfb794c9361de4e1
Коротыч Михаил <m.korotich@saychas.office>
2021-09-13 17:00:58 +0300

    test

--- front/assets/js/common/common.js
+++ front/assets/js/common/common.js
@@ -1,30 +1,14 @@
-// function doSetCaretPosition (oField, iCaretPos) {
-//
-//      // IE Support
-//      if (document.selection) {
-//
-//        // Set focus on the element
-//        oField.focus ();
-//
-//        // Create empty selection range
-//        var oSel = document.selection.createRange ();
-//
-//        // Move selection start and end to 0 position
-//        oSel.moveStart ('character', -oField.value.length);
-//
-//        // Move selection start and end to desired position
-//        oSel.moveStart ('character', iCaretPos);
-//        oSel.moveEnd ('character', 0);
-//        oSel.select ();
-//      }
-//
-//      // Firefox support
-//      else if (oField.selectionStart || oField.selectionStart == '0') {
-//        oField.selectionStart = iCaretPos;
-//        oField.selectionEnd = iCaretPos;
-//        oField.focus ();
-//      }
-// }
+$.fn.setCursorPosition = function(pos) {
+    if ($(this).get(0).setSelectionRange) {
+      $(this).get(0).setSelectionRange(pos, pos);
+    } else if ($(this).get(0).createTextRange) {
+      var range = $(this).get(0).createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', pos);
+      range.moveStart('character', pos);
+      range.select();
+    }
+  };
 
 $(document).ready(function () {
     showBasket(0);
@@ -32,9 +16,7 @@ $(document).ready(function () {
     $(".phoneinput").mask("+7 (999) 999-99-99");
 
     $(document).on('click','.phoneinput',function(){
-      if ($(this).val() === '+7 (___) ___-__-__') {
-        $(this).get(0).setSelectionRange(0, 0);
-      }
+      $(this).setCursorPosition(3);
     });
 
     window.onbeforeunload = function () {
