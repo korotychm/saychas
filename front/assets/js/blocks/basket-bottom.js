e711e8d08b33edf43fce50162d687c357ec2fae1
Коротыч Михаил <m.korotich@saychas.office>
2021-09-13 16:21:36 +0300

    basket top update

--- front/assets/js/blocks/basket-bottom.js
+++ front/assets/js/blocks/basket-bottom.js
@@ -26,3 +26,19 @@ function showBasket(productadd = 0) {
 $(document).on("click", ".basketbottom__close", function () {
     $("#basketbottom").removeClass('active');
 });
+
+$(document).ready(function(){
+  $.ajax({
+      url: "/ajax/add-to-basket",
+      cache: false,
+      type: 'POST',
+      //dataType: 'json',
+      data: {"product": 0},
+      success: function (data) {
+          $("#zakazcount").html(data.count); //data.total
+      },
+      error: function (xhr, ajaxOptions, thrownError) {
+          $("#basketbottom .basketbottom__content").html("Ошибка соединения " + xhr.status + ", попробуйте повторить попытку позже." + "<hr> " + xhr.status + " " + thrownError);
+      }
+  });
+})
