1ff85ec1fc362b70f1f2812ad6d84ab507a7bb37
Коротыч Михаил <m.korotich@saychas.office>
2021-09-13 16:51:46 +0300

    caret pos phone input

--- front/assets/js/common/common.js
+++ front/assets/js/common/common.js
@@ -1,7 +1,43 @@
+function doSetCaretPosition (oField, iCaretPos) {
+
+     // IE Support
+     if (document.selection) {
+
+       // Set focus on the element
+       oField.focus ();
+
+       // Create empty selection range
+       var oSel = document.selection.createRange ();
+
+       // Move selection start and end to 0 position
+       oSel.moveStart ('character', -oField.value.length);
+
+       // Move selection start and end to desired position
+       oSel.moveStart ('character', iCaretPos);
+       oSel.moveEnd ('character', 0);
+       oSel.select ();
+     }
+
+     // Firefox support
+     else if (oField.selectionStart || oField.selectionStart == '0') {
+       oField.selectionStart = iCaretPos;
+       oField.selectionEnd = iCaretPos;
+       oField.focus ();
+     }
+}
+
 $(document).ready(function () {
     showBasket(0);
     $(".overcover").delay(500).fadeOut("slow");
     $(".phoneinput").mask("+7 (999) 999-99-99");
+
+    $(".phoneinput").focusin(function(){
+
+    });
+
+    $(document).on('focusin','.phoneinput',function(){
+       doSetCaretPosition($(this), 4);
+    });
 
     window.onbeforeunload = function () {
         $(".overcover").stop().fadeIn();
