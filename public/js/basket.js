c050e03858807b987aa999052de7362a4003e989
Коротыч Михаил <m.korotich@saychas.office>
2021-09-14 13:26:59 +0300

    test

--- public/js/basket.js
+++ public/js/basket.js
@@ -378,7 +378,9 @@ function waitingOrderStatusVerification(orderId, idInterval = false) {
         success: function (data) {
             if (data.order_status == 0) {
                 //showServicePopupWindow("Готовим заказ к оплате", "<b>Текущий статус: " + data.order_status + "</b><hr>" + date + "<pre>" + JSON.stringify(data, true, 2) + "</pre>", "", true);
-                showServicePopupWindow("Готовим заказ к оплате", clockTemplate, "", true);
+                if ($('#ServiceModalWindow .popup__heading').text() != 'Готовим заказ к оплате'){
+                  showServicePopupWindow("Готовим заказ к оплате", clockTemplate, "", true);
+                }
             } else {
                 //showServicePopupWindow("Заказ полностью сформирован", "переходим на страницу оплаты", '<button class="changed-products__btn formsendbutton" onclick="orderPayTinkoff(\'' + orderId +'\')">Оплатить заказ</div>');
                 if (idInterval != false) {
