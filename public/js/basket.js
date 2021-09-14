41befc57422d84e6931a6b6eb5633b89f9158238
Коротыч Михаил <m.korotich@saychas.office>
2021-09-14 13:31:27 +0300

    test

--- public/js/basket.js
+++ public/js/basket.js
@@ -342,32 +342,33 @@ function whatHappened(noclose = false) {
     });
 }
 
+const clockTemplate = `<div class='waiting'>
+                        <svg width="73px" height="88px" viewBox="0 0 73 88" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                          <g id="hourglass">
+                          <path d="M63.8761664,86 C63.9491436,84.74063 64,83.4707791 64,82.1818182 C64,65.2090455 57.5148507,50.6237818 48.20041,44 C57.5148507,37.3762182 64,22.7909545 64,5.81818182 C64,4.52922091 63.9491436,3.25937 63.8761664,2 L10.1238336,2 C10.0508564,3.25937 10,4.52922091 10,5.81818182 C10,22.7909545 16.4851493,37.3762182 25.79959,44 C16.4851493,50.6237818 10,65.2090455 10,82.1818182 C10,83.4707791 10.0508564,84.74063 10.1238336,86 L63.8761664,86 Z" id="glass" fill="#f2f2f2"></path>
+                          <rect id="top-plate" fill="#252525" x="0" y="0" width="74" height="8" rx="2"></rect>
+                          <rect id="bottom-plate" fill="#252525" x="0" y="80" width="74" height="8" rx="2"></rect>
+                          <g id="top-sand" transform="translate(18, 21)">
+                          <clipPath id="top-clip-path" fill="white">
+                          <rect x="0" y="0" width="38" height="21"></rect>
+                          </clipPath>
+                          <path fill="#ff4b2d" clip-path="url(#top-clip-path)" d="M38,0 C36.218769,7.51704545 24.818769,21 19,21 C13.418769,21 1.9,7.63636364 0,0 L38,0 Z"></path>
+                          </g>
+                          <g id="bottom-sand" transform="translate(18, 55)">
+                          <clipPath id="bottom-clip-path" fill="white">
+                          <rect x="0" y="0" width="38" height="21"></rect>
+                          </clipPath>
+                          <g clip-path="url(#bottom-clip-path)">
+                          <path fill="#ff4b2d" d="M0,21 L38,21 C36.1,13.3636364 24.581231,0 19,0 C13.181231,0 1.781231,13.4829545 0,21 Z"></path>
+                          </g>
+                          </g>
+                          </g>
+                        </svg>
+                      </div>`;
+
 function waitingOrderStatusVerification(orderId, idInterval = false) {
     var date = new Date();
     status = 5;
-    let clockTemplate = `<div class='waiting'>
-                            <svg width="73px" height="88px" viewBox="0 0 73 88" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                              <g id="hourglass">
-                              <path d="M63.8761664,86 C63.9491436,84.74063 64,83.4707791 64,82.1818182 C64,65.2090455 57.5148507,50.6237818 48.20041,44 C57.5148507,37.3762182 64,22.7909545 64,5.81818182 C64,4.52922091 63.9491436,3.25937 63.8761664,2 L10.1238336,2 C10.0508564,3.25937 10,4.52922091 10,5.81818182 C10,22.7909545 16.4851493,37.3762182 25.79959,44 C16.4851493,50.6237818 10,65.2090455 10,82.1818182 C10,83.4707791 10.0508564,84.74063 10.1238336,86 L63.8761664,86 Z" id="glass" fill="#f2f2f2"></path>
-                              <rect id="top-plate" fill="#252525" x="0" y="0" width="74" height="8" rx="2"></rect>
-                              <rect id="bottom-plate" fill="#252525" x="0" y="80" width="74" height="8" rx="2"></rect>
-                              <g id="top-sand" transform="translate(18, 21)">
-                              <clipPath id="top-clip-path" fill="white">
-                              <rect x="0" y="0" width="38" height="21"></rect>
-                              </clipPath>
-                              <path fill="#ff4b2d" clip-path="url(#top-clip-path)" d="M38,0 C36.218769,7.51704545 24.818769,21 19,21 C13.418769,21 1.9,7.63636364 0,0 L38,0 Z"></path>
-                              </g>
-                              <g id="bottom-sand" transform="translate(18, 55)">
-                              <clipPath id="bottom-clip-path" fill="white">
-                              <rect x="0" y="0" width="38" height="21"></rect>
-                              </clipPath>
-                              <g clip-path="url(#bottom-clip-path)">
-                              <path fill="#ff4b2d" d="M0,21 L38,21 C36.1,13.3636364 24.581231,0 19,0 C13.181231,0 1.781231,13.4829545 0,21 Z"></path>
-                              </g>
-                              </g>
-                              </g>
-                            </svg>
-                          </div>`;
     $.ajax({
         beforeSend: function () {
         },
@@ -378,8 +379,8 @@ function waitingOrderStatusVerification(orderId, idInterval = false) {
         success: function (data) {
             if (data.order_status == 0) {
                 //showServicePopupWindow("Готовим заказ к оплате", "<b>Текущий статус: " + data.order_status + "</b><hr>" + date + "<pre>" + JSON.stringify(data, true, 2) + "</pre>", "", true);
-                if ($('#ServiceModalWindow .popup__heading').text() != 'Готовим заказ к оплате'){
-                  showServicePopupWindow("Готовим заказ к оплате", clockTemplate, "", true);
+                if ($('#ServiceModalWindow .popup__heading').text() != 'Готовим ваш заказ к оплате'){
+                  showServicePopupWindow("Готовим ваш заказ к оплате", clockTemplate, "", true);
                 }
             } else {
                 //showServicePopupWindow("Заказ полностью сформирован", "переходим на страницу оплаты", '<button class="changed-products__btn formsendbutton" onclick="orderPayTinkoff(\'' + orderId +'\')">Оплатить заказ</div>');
@@ -441,7 +442,8 @@ function sendBasketData() {
         data: $("#user-basket-form").serialize(),
         success: function (data) {
             //console.log(data)
-            showServicePopupWindow("Формируем заказ", JSON.stringify(data));
+            //showServicePopupWindow("Формируем заказ...", JSON.stringify(data));
+            showServicePopupWindow("Формируем заказ...", clockTemplate);
             if (data.result == true) {
                 //    location = "/client-orders";
                 //console.log(data);
