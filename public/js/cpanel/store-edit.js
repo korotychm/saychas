0d38c8e0d98c34d26818cdcd69d51735f9ae2bd2
Коротыч Михаил <m.korotich@saychas.office>
2021-09-15 11:08:56 +0300

    test

--- public/js/cpanel/store-edit.js
+++ public/js/cpanel/store-edit.js
@@ -30,6 +30,56 @@ const StoreEdit = {
                         <p>Статус устанавливает глобальный режим работы магазина.<br> Для изменения работы в определенные дни - восользуйтесь полями ниже.</p>
                       </div>
                     </div>
+                    <div class="store__timetable">
+                      <div class="store__timetable-inputs">
+                        <div class="store__timetable-main active">
+                          <div class="product__attribute">
+                            <h2>Рабочие дни</h2>
+                            <div class="input-group">
+                              <div>
+                                <input type="text" class="input timeinput" />
+                              </div>
+                              <div>
+                                <input type="text" class="input timeinput" />
+                              </div>
+                            </div>
+                          </div>
+                          <div class="product__attribute">
+                            <h2>Суббота</h2>
+                            <div class="input-group">
+                              <div>
+                                <input type="text" class="input timeinput" />
+                              </div>
+                              <div>
+                                <input type="text" class="input timeinput" />
+                              </div>
+                            </div>
+                          </div>
+                          <div class="product__attribute">
+                            <h2>Воскресенье</h2>
+                            <div class="input-group">
+                              <div>
+                                <input type="text" class="input timeinput" />
+                              </div>
+                              <div>
+                                <input type="text" class="input timeinput" />
+                              </div>
+                            </div>
+                          </div>
+                          <div class="product__attribute">
+                            <h2>Праздничные дни</h2>
+                            <div class="input-group">
+                              <div>
+                                <input type="text" class="input timeinput" />
+                              </div>
+                              <div>
+                                <input type="text" class="input timeinput" />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
                 <div v-else class="product__error">Вы не можете редактировать этот магазин</div>
