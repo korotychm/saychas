5d6898f33b2cdd553ad8fde209d730528fb87ccf
Коротыч Михаил <m.korotich@saychas.office>
2021-09-15 10:17:17 +0300

    test

--- public/js/cpanel/store-edit.js
+++ public/js/cpanel/store-edit.js
@@ -12,8 +12,10 @@ const StoreEdit = {
                     </div>
                     <div class="product__attribute">
                       <h2>Комментарий</h2>
-                      <input type="text" class="input" />
-                      <p>Комментарий для курьера — что бы легче находить и быстрее приезжать</p>
+                      <div>
+                        <input type="text" class="input" />
+                        <p>Комментарий для курьера — что бы легче находить и быстрее приезжать</p>
+                      </div>
                     </div>
                   </div>
                 </div>
