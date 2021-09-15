f9ff6f13f042d0b32813dc1d78f778e34aa00109
Коротыч Михаил <m.korotich@saychas.office>
2021-09-15 10:29:19 +0300

    test

--- public/js/cpanel/store-edit.js
+++ public/js/cpanel/store-edit.js
@@ -18,6 +18,19 @@ const StoreEdit = {
                       </div>
                     </div>
                   </div>
+                  <div class="store__fields">
+                    <div class="product__attribute">
+                      <h2>Статус работы</h2>
+                      <div>
+                        <select class="select" value="1">
+                          <option value="1">Работает по графику</option>
+                          <option value="2">Временно не работает</option>
+                          <option value="2">Закрыт</option>
+                        </select>
+                      </div>
+                      <p>Статус устанавливает глобальный режим работы магазина.<br> Для изменения работы в определенные дни - восользуйтесь полями ниже.</p>
+                    </div>
+                  </div>
                 </div>
                 <div v-else class="product__error">Вы не можете редактировать этот магазин</div>
               </div>
