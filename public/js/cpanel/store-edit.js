4016948c5ec9a1e90cf63f9fbb3564c2afc2bb60
Коротыч Михаил <m.korotich@saychas.office>
2021-09-15 10:08:31 +0300

    store edit

--- public/js/cpanel/store-edit.js
+++ public/js/cpanel/store-edit.js
@@ -1,3 +1,26 @@
 const StoreEdit = {
-  template: `<div>Магазин с id {{ $route.params.id }}</div>`
+  template: `<div class="cp-container store">
+                <div v-if="editable">
+                  <div class="product__attribute  product__attribute--short">
+                    <h2>Название магазина</h2>
+                    <input type="text" class="input" />
+                  </div>
+                  <div class="product__attribute">
+                    <h2>Адрес</h2>
+                    <input type="text" class="input" />
+                  </div>
+                  <div class="product__attribute">
+                    <h2>Комментарий</h2>
+                    <input type="text" class="input" />
+                  </div>
+                </div>
+                <div v-else class="product__error">Вы не можете редактировать этот магазин</div>
+              </div>
+            `,
+  data: function () {
+    return {
+      editable: true
+    }
+  },
+
 }
