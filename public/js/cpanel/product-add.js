1ae3765717cdf918fd5949e37f9246bc94da74ac
Коротыч Михаил <m.korotich@saychas.office>
2021-09-15 17:31:03 +0300

    test

--- public/js/cpanel/product-add.js
+++ public/js/cpanel/product-add.js
@@ -1,5 +1,16 @@
 const ProductAdd = {
   template: `<div>
+                <div class="filter">
+                  <div class="filter__btn">
+                      <a class="btn btn--primary">По одному товару</a>
+                  </div>
+                  <div class="filter__btn">
+                      <router-link to="/product-add-file" class="btn btn--secondary">Массой товаров</router-link>
+                  </div>
+                  <div class="filter__btn">
+                      <router-link to="/product-add-api" class="btn btn--secondary">Загрузка по API</router-link>
+                  </div>
+                </div>
                 <div class="cp-container product"></div>
             </div>`,
   mounted: function(){
