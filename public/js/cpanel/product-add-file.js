569aecdd0304a167d1feb62e8acdaefb8c95cc36
Коротыч Михаил <m.korotich@saychas.office>
2021-09-15 17:52:19 +0300

    add files layout

--- public/js/cpanel/product-add-file.js
+++ public/js/cpanel/product-add-file.js
@@ -11,7 +11,27 @@ const ProductAddFile = {
                       <router-link to="/product-add-api" class="btn btn--secondary">Загрузка по API</router-link>
                   </div>
                 </div>
-                <div class="cp-container product"></div>
+                <div class="cp-container product product-add-file">
+                  <div class="product__category">
+                      <h2>Категория</h2>
+                      <div class="search-select">
+                          <input class="input search-select__input" type="text" value="" />
+                      </div>
+                  </div>
+                  <div class="product-add-file__files">
+                    <div class="product-add-file__files-download">
+                      <a href="#">Скачать файл</a>
+                      <p>Скачайте и заполните файл.</p>
+                      <p>Чем больше полей заполните - тем легче пользователям будет найти ваш товар.</p>
+                    </div>
+                    <div class="product-add-file__files-upload">
+                      <input type="file" id="upload-file"/>
+                      <label for="upload-file">Загрузить файл</label>
+                      <p>Загрузите заполненный файл.</p>
+                      <p>Товары появятся на сайте после обработки.</p>
+                    </div>
+                  </div>
+                </div>
             </div>`,
   mounted: function(){
     $('.main__loader').hide();
