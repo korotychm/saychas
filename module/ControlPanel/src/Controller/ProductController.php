a2a45c5325419248ef960bc1c209faa32ad432ae
Коротыч Михаил <m.korotich@saychas.office>
2021-09-16 15:44:37 +0300

    test

--- module/ControlPanel/src/Controller/ProductController.php
+++ module/ControlPanel/src/Controller/ProductController.php
@@ -191,9 +191,9 @@ class ProductController extends AbstractActionController
     {
         $post = $this->getRequest()->getPost()->toArray();
         $product = $post['data']['product'];
-        echo '<pre>';
-        print_r($product);
-        echo '</pre>';
+        // echo '<pre>';
+        // print_r($product);
+        // echo '</pre>';
         $result = ['matched_count' => 0, 'modified_count' => 0];
         if($this->canUpdateProduct($product)) {
             $result = $this->productManager->replaceProduct($product);
