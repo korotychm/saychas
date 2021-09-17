f990b9d400f41882cb0abde6b767e8999997f0ea
Коротыч Михаил <m.korotich@saychas.office>
2021-09-17 15:54:30 +0300

    test

--- public/js/cpanel/product-add.js
+++ public/js/cpanel/product-add.js
@@ -269,18 +269,6 @@ const ProductAdd = {
               this.flatCategories();
             }
           })
-          .catch(error => {
-            if (error.response.status == '403'){
-              this.htmlContent = error403template;
-            }
-            if (error.response.status == '404'){
-              this.htmlContent = error404template;
-            }
-            if (error.response.status == '500'){
-              this.htmlContent = error500template;
-            }
-            $('.main__loader').hide();
-          });
     },
     checkCategory() {
       if (!this.categorySearch){
