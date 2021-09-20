3b125627cbca1f9df4639e3891c8cdf815383313
Коротыч Михаил <m.korotich@saychas.office>
2021-09-20 17:16:27 +0300

    test

--- public/js/cpanel/store-add.js
+++ public/js/cpanel/store-add.js
@@ -234,7 +234,7 @@ const StoreAdd = {
   mounted: function(){
     setTimeout(() => {
       this.storeDaData();
-      $('.phoneinput').mask('+7 (999) 999-99-99');
+      //$('.phoneinput').mask('+7 (999) 999-99-99');
       $('.timeinput').mask('99:99');
     }, 200);
     $('.main__loader').hide();
