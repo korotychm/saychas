8b6a140a9357564b5467ecdcd389a00e4127b684
Коротыч Михаил <m.korotich@saychas.office>
2021-09-22 09:44:17 +0300

    test

--- public/js/cpanel/products.js
+++ public/js/cpanel/products.js
@@ -21,7 +21,7 @@ const Products = {
                 </label>
                 <label v-for="category in filters.categories" class="custom-select__option">
                   <input type="radio" :checked="(category[0] === selectedFilters.category_id)" :value="category[0]" name="category_filter" v-model="selectedFilters.category_id" @change="loadPage()" />
-                  <span>{{status[1]}}</span>
+                  <span>{{category[1]}}</span>
                 </label>
               </div>
             </div>
