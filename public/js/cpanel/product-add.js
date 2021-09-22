288236bac44dd11710ed5d56d3d53a537bf46a63
Коротыч Михаил <m.korotich@saychas.office>
2021-09-22 11:50:37 +0300

    test

--- public/js/cpanel/product-add.js
+++ public/js/cpanel/product-add.js
@@ -167,7 +167,7 @@ const ProductAdd = {
                                   </div>
                                   <div class="product__images">
                                       <div class="product__attribute">
-                                          <h2 :class="{'input-error' : (!product.images.length && errors)}">Фото товара <span>Рекомендуемый размер <br>фото — 1000х1000 px. </span><span>Вы можете загрузить до 8 фотографий.</span></h2>
+                                          <h2><tag :class="{'input-error' : (!product.images.length && errors)}">Фото товара <span class="required">*</span></tag> <p>Рекомендуемый размер <br>фото — 1000х1000 px. </p><p>Вы можете загрузить до 8 фотографий.</p></h2>
                                           <div class="product__images-wrap">
                                               <div class="product__images-nav"><button class="product__images-arrow product__images-arrow--up disabled" data-shift="-1"></button>
                                                   <div class="product__images-list product__images-list--slider">
