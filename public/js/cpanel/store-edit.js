ca0f2f77694e48d4ef62e2fb92d9c6ac4541f33e
Коротыч Михаил <m.korotich@saychas.office>
2021-09-23 17:18:37 +0300

    test

--- public/js/cpanel/store-edit.js
+++ public/js/cpanel/store-edit.js
@@ -134,7 +134,7 @@ const StoreEdit = {
                           </div>
                         </div>
                         <div class="store__calendar">
-                          <v-date-picker v-model='selectedDate' :min-date='new Date()' @update:from-page="modifiedDaysHighlight" />
+                          <v-date-picker v-model='selectedDate' :min-date='new Date()' @update:from-page="modifiedDaysHighlight" @update:to-page="modifiedDaysHighlight" />
                         </div>
                       </div>
                       <p>Если магазин работает круглосуточно - проставьте с 00:00 до 23:59</p>
