68e951a2236d7eebbaaf7664803274a5cf0bf69a
Коротыч Михаил <m.korotich@saychas.office>
2021-09-24 17:34:50 +0300

    test

--- public/js/providers.js
+++ public/js/providers.js
@@ -1,34 +1,34 @@
-function getProdeuctCategory(){
-    var brandId = $("#baseId").val();
-    var categoryId = $("#categoryId").val();
-
-    //$("#quick-menu-item-" + categoryId ).addClass("active");
-    $.ajax({
-            beforeSend : function (){
-                },
-            url: "/ajax-get-products-provider",
-            type: 'POST',
-            cache: false,
-            data: {"providerId": brandId, "categoryId": categoryId },
-            success: function (data) {
-             //showAjaxErrorPopupWindow ("id", categoryId );
-             $("#providercontent").html(JSON.stringify(data,true,2));
-              return false;
-            },
-            error: function (xhr, ajaxOptions, thrownError) {
-             if (xhr.status !== 0) {
-                    showAjaxErrorPopupWindow (xhr.status, thrownError);
-                }
-                return false;
-            }
-        });
-    return false;
-}
-
-$(document).ready(function () {
-  getProdeuctCategory();
-
-});
+// function getProdeuctCategory(){
+//     var brandId = $("#baseId").val();
+//     var categoryId = $("#categoryId").val();
+//
+//     //$("#quick-menu-item-" + categoryId ).addClass("active");
+//     $.ajax({
+//             beforeSend : function (){
+//                 },
+//             url: "/ajax-get-products-provider",
+//             type: 'POST',
+//             cache: false,
+//             data: {"providerId": brandId, "categoryId": categoryId },
+//             success: function (data) {
+//              //showAjaxErrorPopupWindow ("id", categoryId );
+//              $("#providercontent").html(JSON.stringify(data,true,2));
+//               return false;
+//             },
+//             error: function (xhr, ajaxOptions, thrownError) {
+//              if (xhr.status !== 0) {
+//                     showAjaxErrorPopupWindow (xhr.status, thrownError);
+//                 }
+//                 return false;
+//             }
+//         });
+//     return false;
+// }
+//
+// $(document).ready(function () {
+//   getProdeuctCategory();
+//
+// });
 
 $(document).ready(function(){
 
@@ -48,7 +48,7 @@ $(document).ready(function(){
             .post('/ajax-get-products-provider',
               Qs.stringify({
                 categoryId : this.categoryId,
-                providerId: this.storeId
+                providerId: this.providerId
               }))
             .then(response => {
               console.log(response);
