f55ae5ba1c5320725554740fe44f0b7bd0d5b444
Коротыч Михаил <m.korotich@saychas.office>
2021-09-24 15:01:35 +0300

    test

--- public/js/categories.js
+++ public/js/categories.js
@@ -1,29 +1,57 @@
-function getProdeuctCategory(){
-    var categoryId = window.location.href.split("/").slice(-1)[0]; 
-    $("#quick-menu-item-" + categoryId ).addClass("active");
-    $.ajax({
-            beforeSend : function (){
-                },
-            url: "/ajax-get-products-categories",
-            type: 'POST',
-            cache: false,
-            data: {"categoryId": categoryId},
-            success: function (data) {
-             //showAjaxErrorPopupWindow ("id", categoryId );
-             $("#category-list").html(JSON.stringify(data,true,2));
-              return false;
-            },
-            error: function (xhr, ajaxOptions, thrownError) {
-             if (xhr.status !== 0) {
-                    showAjaxErrorPopupWindow (xhr.status, thrownError);
-                }    
-                return false;
-            }
-        });
-    return false;    
-}
+// function getProdeuctCategory(){
+//     var categoryId = window.location.href.split("/").slice(-1)[0];
+//     $("#quick-menu-item-" + categoryId ).addClass("active");
+//     $.ajax({
+//             beforeSend : function (){
+//                 },
+//             url: "/ajax-get-products-categories",
+//             type: 'POST',
+//             cache: false,
+//             data: {"categoryId": categoryId},
+//             success: function (data) {
+//              //showAjaxErrorPopupWindow ("id", categoryId );
+//              $("#category-list").html(JSON.stringify(data,true,2));
+//               return false;
+//             },
+//             error: function (xhr, ajaxOptions, thrownError) {
+//              if (xhr.status !== 0) {
+//                     showAjaxErrorPopupWindow (xhr.status, thrownError);
+//                 }
+//                 return false;
+//             }
+//         });
+//     return false;
+// }
+//
+// $(document).ready(function () {
+//   getProdeuctCategory();
+//
+// });
 
-$(document).ready(function () {
-  getProdeuctCategory();
-    
+$(document).ready(function(){
+
+  if ($('#category-page').length){
+
+    var categoryPage = new Vue({
+      el: '#category-page',
+      data: {
+        category_id: '',
+        products: []
+      },
+      created() {
+          this.category_id = window.location.href.split("/").slice(-1)[0],
+          axios
+            .post('/ajax-get-products-categories',
+              Qs.stringify({
+                categoryId : this.category_id
+              }))
+            .then(response => {
+              this.products = response.data.products;
+              console.log('Products', this.products);
+            });
+      }
+    });
+
+  }
+
 });
