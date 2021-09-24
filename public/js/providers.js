b18a5ec25c71150baab23645668503f8c616c0ff
Коротыч Михаил <m.korotich@saychas.office>
2021-09-24 17:30:18 +0300

    test

--- public/js/providers.js
+++ public/js/providers.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
   if ($('#provider-page').length){
 
     var categoryPage = new Vue({
-      el: '#store-page',
+      el: '#provider-page',
       data: {
         products: [],
         length: 0,
