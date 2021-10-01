47f33572a4caa47da76dd356dfda5c006664c126
Plusweb <plusw.ru@gmail.com>
2021-10-01 10:22:29 +0300

    fix

--- module/Application/src/Service/HtmlProviderService.php
+++ module/Application/src/Service/HtmlProviderService.php
@@ -632,6 +632,7 @@ class HtmlProviderService {
             }
             $return["product"][$returnprefix] = [
                 "provider_id" => $provider_store_id, //$prov,
+                "provider_main_id" => $prov,
                 "availblechek" => $availblechek[$prov],
                 "provider_disable" => $provider_disable,
                 "provider_name" => $provider->getTitle(),
