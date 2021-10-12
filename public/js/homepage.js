d3a04ecbcd71040978c65056df20e74054a04fdd
Коротыч Михаил <m.korotich@saychas.office>
2021-10-12 13:50:01 +0300

    test

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -72,15 +72,7 @@ $(document).ready(function(){
             });
       },
       updated() {
-        if (this.length){
-          $('#viewed-products .products-carousel').slick(
-            {
-              infinite: true,
-              slidesToShow: 4,
-              slidesToScroll: 4
-            }
-          );
-        }
+        
       }
     });
 
