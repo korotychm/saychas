ea192f3904234f0d7827c5f5143bc9b0484bd607
Коротыч Михаил <m.korotich@saychas.office>
2021-10-13 10:04:44 +0300

    TesT

--- public/js/cpanel/inventory.js
+++ public/js/cpanel/inventory.js
@@ -112,6 +112,7 @@ const Inventory = {
   methods: {
     setTitle(title){
       this.$route.meta.h1 = 'Товарные остатки - ' + title;
+      console.log(this.$route.meta.h1);
     },
     saveProduct(index) {
       let requestUrl = '/control-panel/update-stock-balance';
