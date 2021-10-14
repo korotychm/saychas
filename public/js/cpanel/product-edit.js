8dbfe33d7e0be68c7f72901d85d22c24aa92a80e
mvkorotych <korotychm@gmail.com>
2021-10-14 15:21:05 +0300

    prevent illegal symbold input

--- public/js/cpanel/product-edit.js
+++ public/js/cpanel/product-edit.js
@@ -366,9 +366,9 @@ const ProductEdit = {
       }
       let output = input;
       for (var i = 0; i < input.length; i++) {
-            let c = input.charCodeAt(i);
+            let c = input.charCodeAt(i);s
             console.log('charCode',c,'Symbol',input[i]);
-            if (c < 32 || c == 39 || c == 96 || (c > 255 && c < 1072) || c > 1103) {
+            if (c < 32 || c == 39 || c == 96 || (c > 255 && c < 1040) || c > 1103) {
                 output = output.replace(input[i],'');
             }
       }
