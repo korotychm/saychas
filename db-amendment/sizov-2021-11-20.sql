ed7005f3763e55f82ca67bfe67f6afcc50d97225
Plusweb <plusw.ru@gmail.com>
2021-10-20 17:12:25 +0300

    mreged korotich

---
+++ db-amendment/sizov-2021-11-20.sql
@@ -0,0 +1,10 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  plusweb
+ * Created: Oct 20, 2021
+ */
+
+ALTER TABLE `category` ADD `url` VARCHAR(150) NULL DEFAULT NULL AFTER `sort_order`; 
