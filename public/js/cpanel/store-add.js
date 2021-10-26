3256b237d314ca4103831d2b6d66bf07bbed5ba0
Коротыч Михаил <m.korotich@saychas.office>
2021-10-26 15:14:21 +0300

    test

--- public/js/cpanel/store-add.js
+++ public/js/cpanel/store-add.js
@@ -400,8 +400,8 @@ const StoreAdd = {
             .then(response => {
               console.log('Добавление магазина',response);
               if (response.data.result){
-                showMessage('Магазин добавлен!');
-                router.replace('/stores');
+                //showMessage('Магазин добавлен!');
+                //router.replace('/stores');
               } else {
                 showServicePopupWindow('Невозможно добавить магазин', response.data.data.error_description_for_user);
               }
