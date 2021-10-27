14329ce7cd197f8dc85a9f79f26c6643c7562b9b
Коротыч Михаил <m.korotich@saychas.office>
2021-10-27 16:14:07 +0300

    test

--- public/js/cpanel/orders.js
+++ public/js/cpanel/orders.js
@@ -58,17 +58,17 @@ const Orders = {
                                 <div class="orders__product-count-initial" v-if="product.qty_partner < product.qty"><b>{{ product.qty }}</b> шт</div>
                                 <div class="orders__product-count-actual"><b>{{ product.qty_partner }}</b> шт</div>
                                 <div class="orders__product-count-edit" v-show="product.product_id === activeItem">
-                                  <input type="number" value="3">
+                                  <input type="number" v-model="currentQuantity">
                                 </div>
                             </div>
                             <div class="orders__product-sum">{{ calculatePrice(product.price, product.discount).toLocaleString() }} ₽</div>
                             <div class="orders__product-edit">
-                              <button v-if="product.product_id !== activeItem" @click="activeItem = product.product_id">
+                              <button v-if="product.product_id !== activeItem" @click="activeItem = product.product_id; currentQuantity = product.qty_partner">
                                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="14px" height="14px">
                                   <path fill-rule="evenodd" fill="rgb(255, 75, 45)" d="M13.352,4.166 L12.750,4.778 C12.591,4.938 12.334,4.938 12.175,4.778 L9.291,1.858 C9.133,1.699 9.133,1.440 9.291,1.280 L9.291,1.280 L9.901,0.663 C10.766,0.214 12.173,0.221 13.46,0.648 C13.51,0.653 13.56,0.659 13.61,0.663 L13.64,0.663 L13.352,0.955 C14.225,1.845 14.225,3.277 13.352,4.166 ZM11.312,6.241 L5.123,12.523 C4.715,12.938 4.199,13.226 3.635,13.355 L0.735,14.12 C0.404,14.83 0.78,13.871 0.7,13.537 C0.13,13.438 0.10,13.335 0.17,13.237 L0.842,10.394 C0.986,9.900 1.250,9.449 1.610,9.83 L7.860,2.741 C8.19,2.581 8.276,2.581 8.435,2.741 L11.312,5.662 C11.469,5.822 11.469,6.80 11.312,6.241 Z" />
                                 </svg>
                               </button>
-                              <button class="done" v-if="product.product_id === activeItem">
+                              <button class="done" v-if="product.product_id === activeItem" @click="product.qty_partner = currentQuantity; activeItem = null;">
                                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="22px" height="15px">
                                   <path fill-rule="evenodd" fill="rgb(255, 75, 45)" d="M3.343,6.720 L8.778,12.155 C9.363,12.741 9.363,13.691 8.778,14.276 C8.192,14.862 7.242,14.862 6.656,14.276 L1.221,8.842 C0.636,8.255 0.636,7.306 1.221,6.720 C1.807,6.134 2.757,6.134 3.343,6.720 Z"></path>
                                   <path fill-rule="evenodd" fill="rgb(255, 75, 45)" d="M20.571,2.487 L9.519,13.540 C8.950,14.109 8.27,14.109 7.458,13.540 C6.889,12.970 6.889,12.47 7.458,11.479 L18.510,0.426 C19.80,0.142 20.2,0.142 20.571,0.426 C21.140,0.995 21.140,1.918 20.571,2.487 Z"></path>
