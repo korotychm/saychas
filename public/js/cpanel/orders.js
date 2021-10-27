124857fe7fc4d22f6d5c65466ded456e8918ed6c
Коротыч Михаил <m.korotich@saychas.office>
2021-10-27 16:23:26 +0300

    test

--- public/js/cpanel/orders.js
+++ public/js/cpanel/orders.js
@@ -40,7 +40,7 @@ const Orders = {
                     <div class="td orders__status">{{ order.status }}</div>
                     <div class="td orders__short" v-show="activeOrder !== index">
                         <div class="orders__images" v-if="order.items.length">
-                            <div class="orders__image" v-for="product in order.items">
+                            <div class="orders__image" :class="{'orders__image--disabled: product.qty_partner == 0'}" v-for="product in order.items">
                               <img :src="product.image" />
                             </div>
                         </div>
@@ -104,7 +104,8 @@ const Orders = {
           },
           filtersCreated: false,
           activeOrder: null,
-          activeItem: null
+          activeItem: null,
+          currentQuantity: null
         }
     },
     methods: {
