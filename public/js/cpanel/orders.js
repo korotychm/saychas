4a506926bc2a06838d6ed5a4c189d2237f9e3319
Коротыч Михаил <m.korotich@saychas.office>
2021-10-27 15:05:01 +0300

    test

--- public/js/cpanel/orders.js
+++ public/js/cpanel/orders.js
@@ -82,7 +82,7 @@ const Orders = {
         }
       },
       localeDate(ms) {
-        let dateObject = new Date(ms);
+        let dateObject = new Date(ms * 100);
         return dateObject.toLocaleString();
       },
       getOrders() {
