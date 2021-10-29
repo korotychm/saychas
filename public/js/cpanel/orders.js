5ca2bc702f7511b5b77559cd35280af341c3e549
mvkorotych <korotychm@gmail.com>
2021-10-29 17:11:46 +0300

    test

--- public/js/cpanel/orders.js
+++ public/js/cpanel/orders.js
@@ -184,9 +184,7 @@ const Orders = {
           } else if (+order.status_id == '01'){
             let deadline = this.calulateTime(order.date,this.deadline_collect,this.deadline_collect_last);
             Vue.set(this.orders[i],'deadline',deadline);
-            let blabla = new Date;
-            blabla = +blabla;
-            Vue.set(this.orders[i],'store',order.store);
+            Vue.set(this.orders[i],'deadline',deadline);
             console.log(order.deadline);
             if (!order.deadline){
               //Сделать дополнительный запрос?
