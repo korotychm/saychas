e3233656ab7f1ab5c97f794a3cd15dea78e65508
Коротыч Михаил <m.korotich@saychas.office>
2021-11-01 13:24:23 +0300

    test

--- public/js/basket.js
+++ public/js/basket.js
@@ -540,10 +540,10 @@ $(function () {
         checkBasketDataBeforeSend();
     });
 
-    // $(document).on('click', '.cart__radio', function () {
-    //     $(this).prev().prop('checked',true);
-    //     setTimepointText();
-    // });
+    $(document).on('click', '.cart__radio', function () {
+        $(this).prev().prop('checked',true);
+        setTimepointText();
+    });
 
     $(document).on('change', '.cart__radio-group > input', function () {
         console.log('123');
