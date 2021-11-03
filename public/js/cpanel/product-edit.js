a6765045477391903a5126a4a121f1187a90e044
Коротыч Михаил <m.korotich@saychas.office>
2021-11-03 14:59:18 +0300

    test

--- public/js/cpanel/product-edit.js
+++ public/js/cpanel/product-edit.js
@@ -9,11 +9,13 @@ const ProductEdit = {
                     <div class="product__attribute">
                         <h2>Статус</h2>
                         <p class="product__status" v-if="product.moderated"><span class="product__status-circle product__status-circle--0"></span>Товар прошел модерацию и опубликован на сайте.</p>
-                        <p class="product__status" v-if="!product.moderated && product.processed">
-                            <span class="product__status-circle product__status-circle--2"></span>
-                            Товар не прошел модерацию. Измените товар и сохраните снова.
-                            <div class="failure-message" style="color: var(--red)" v-html="product.moderation_failure_message"></div>
-                        </p>
+                        <div v-if="!product.moderated && product.processed">
+                          <p class="product__status">
+                              <span class="product__status-circle product__status-circle--2"></span>
+                              Товар не прошел модерацию. Измените товар и сохраните снова.
+                          </p>
+                          <p class="failure-message" style="color: var(--red)" v-html="product.moderation_failure_message"></p>
+                        </div>
                         <p class="product__status" v-if="!product.moderated && !product.processed"><span class="product__status-circle product__status-circle--1"></span>Товар проходит модерацию, вы пока не можете изменять этот товар.</p>
                     </div>
                   </div>
