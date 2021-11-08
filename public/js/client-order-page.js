b1ea67b35b417da75067fb2ed309ca849bd3f85d
Коротыч Михаил <m.korotich@saychas.office>
2021-11-08 11:59:34 +0300

    test

--- public/js/client-order-page.js
+++ public/js/client-order-page.js
@@ -1,28 +1,42 @@
-function getClientOrder(){
-    var orderId = window.location.href.split("/").slice(-1)[0]; 
-    $.ajax({
-            beforeSend: function () {},
-            url: "/ajax-get-order-page",
-            type: 'POST',
-            cache: false,
-            data:{"orderId":orderId},
-            success: function (data) {
-                //if(data.result == 1)
-                $("#client-order").html(JSON.stringify(data,  null, 2 ));
-            },
-            error: function (xhr, ajaxOptions, thrownError) {
-              showServicePopupWindow(
-                            "Ошибка " + xhr.status,
-                            "Ошибка соединения, попробуйте повторить попытку позже." + "\r\n " + xhr.status + " " + thrownError
-                            );
-                return false;
-            }
-        });
-}
+// function getClientOrder(){
+//     var orderId = window.location.href.split("/").slice(-1)[0];
+//     $.ajax({
+//             beforeSend: function () {},
+//             url: "/ajax-get-order-page",
+//             type: 'POST',
+//             cache: false,
+//             data:{"orderId":orderId},
+//             success: function (data) {
+//                 //if(data.result == 1)
+//                 $("#client-order").html(JSON.stringify(data,  null, 2 ));
+//             },
+//             error: function (xhr, ajaxOptions, thrownError) {
+//               showServicePopupWindow(
+//                             "Ошибка " + xhr.status,
+//                             "Ошибка соединения, попробуйте повторить попытку позже." + "\r\n " + xhr.status + " " + thrownError
+//                             );
+//                 return false;
+//             }
+//         });
+// }
 
 $(document).ready(function () {
-    getClientOrder();
+    //getClientOrder();
+    if ($('#profile-order').length){
+      var clientOrders = new Vue({
+        el: '#client-orders',
+        data: {
+          order: userOrder,
+          products: []
+        },
+        methods: {
+          isToday(someDate) {
+              const today = new Date();
+              return someDate.getDate() == today.getDate() &&
+                someDate.getMonth() == today.getMonth() &&
+                someDate.getFullYear() == today.getFullYear()
+          }
+        }
+      });
+    }
 });
-
-
-
--- public/js/client-orders.js
+++ public/js/client-orders.js
@@ -1,26 +1,26 @@
-function getClientOrders(){
-    $.ajax({
-            beforeSend: function () {},
-            url: "/ajax-get-order-list",
-            type: 'POST',
-            cache: false,
-            success: function (data) {
-                //if(data.result == 1)
-                $("#order-list").html(JSON.stringify(data, null, 2 ));
-            },
-            error: function (xhr, ajaxOptions, thrownError) {
-              showServicePopupWindow(
-                            "Ошибка " + xhr.status,
-                            "Ошибка соединения, попробуйте повторить попытку позже." + "\r\n " + xhr.status + " " + thrownError
-                            );
-                return false;
-            }
-        });
-}
-
-$(document).ready(function () {
-    getClientOrders();
-});
+// function getClientOrders(){
+//     $.ajax({
+//             beforeSend: function () {},
+//             url: "/ajax-get-order-list",
+//             type: 'POST',
+//             cache: false,
+//             success: function (data) {
+//                 //if(data.result == 1)
+//                 $("#order-list").html(JSON.stringify(data, null, 2 ));
+//             },
+//             error: function (xhr, ajaxOptions, thrownError) {
+//               showServicePopupWindow(
+//                             "Ошибка " + xhr.status,
+//                             "Ошибка соединения, попробуйте повторить попытку позже." + "\r\n " + xhr.status + " " + thrownError
+//                             );
+//                 return false;
+//             }
+//         });
+// }
+//
+// $(document).ready(function () {
+//     getClientOrders();
+// });
 
 $(document).ready(function () {
 
