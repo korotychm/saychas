3b7c299818aec37f5cf1d6f114bb28df033e82c3
Коротыч Михаил <m.korotich@saychas.office>
2021-11-08 07:31:27 +0300

    test

--- public/js/client-favorites-page.js
+++ public/js/client-favorites-page.js
@@ -1,21 +1,21 @@
-function getClientProducts (type){
-    var url = "/ajax-get-client-" + type;
-     $.ajax({
-            url: url,
-            success: function (data) {
-             $("#client-" + type ).html(JSON.stringify(data,  null, 2 ));
-            },
-            error: function (xhr, ajaxOptions, thrownError) {
-                 showAjaxErrorPopupWindow(xhr.status, thrownError);
-                return false;
-            }
-        });
-}
-
-$(function () {
-    getClientProducts ("favorites");
-    getClientProducts ("history");
-});
+// function getClientProducts (type){
+//     var url = "/ajax-get-client-" + type;
+//      $.ajax({
+//             url: url,
+//             success: function (data) {
+//              $("#client-" + type ).html(JSON.stringify(data,  null, 2 ));
+//             },
+//             error: function (xhr, ajaxOptions, thrownError) {
+//                  showAjaxErrorPopupWindow(xhr.status, thrownError);
+//                 return false;
+//             }
+//         });
+// }
+//
+// $(function () {
+//     getClientProducts ("favorites");
+//     getClientProducts ("history");
+// });
 
 
 $(document).ready(function(){
@@ -114,7 +114,7 @@ $(document).ready(function(){
       },
       updated() {
         setTimeout(() => {
-          $('#viewed-products .products-carousel').slick(
+          $('#user-viewed-products .products-carousel').slick(
             {
               infinite: true,
               slidesToShow: 3,
