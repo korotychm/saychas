6fc487c5aa77f4f591aa5038d456629044d3cebf
Коротыч Михаил <m.korotich@saychas.office>
2021-11-08 13:57:02 +0300

    test

--- public/js/client-order-page.js
+++ public/js/client-order-page.js
@@ -1,28 +1,28 @@
-function getClientOrder(){
-    var orderId = window.location.href.split("/").slice(-1)[0];
-    $.ajax({
-            beforeSend: function () {},
-            url: "/ajax-get-order-page",
-            type: 'POST',
-            cache: false,
-            data:{"orderId":orderId},
-            success: function (data) {
-                //if(data.result == 1)
-                $("#client-order").html(JSON.stringify(data,  null, 2 ));
-                console.log(orderId,JSON.stringify(data,  null, 2 ));
-            },
-            error: function (xhr, ajaxOptions, thrownError) {
-              showServicePopupWindow(
-                            "Ошибка " + xhr.status,
-                            "Ошибка соединения, попробуйте повторить попытку позже." + "\r\n " + xhr.status + " " + thrownError
-                            );
-                return false;
-            }
-        });
-}
+// function getClientOrder(){
+//     var orderId = window.location.href.split("/").slice(-1)[0];
+//     $.ajax({
+//             beforeSend: function () {},
+//             url: "/ajax-get-order-page",
+//             type: 'POST',
+//             cache: false,
+//             data:{"orderId":orderId},
+//             success: function (data) {
+//                 //if(data.result == 1)
+//                 $("#client-order").html(JSON.stringify(data,  null, 2 ));
+//                 console.log(orderId,JSON.stringify(data,  null, 2 ));
+//             },
+//             error: function (xhr, ajaxOptions, thrownError) {
+//               showServicePopupWindow(
+//                             "Ошибка " + xhr.status,
+//                             "Ошибка соединения, попробуйте повторить попытку позже." + "\r\n " + xhr.status + " " + thrownError
+//                             );
+//                 return false;
+//             }
+//         });
+// }
 
 $(document).ready(function () {
-    getClientOrder();
+    //getClientOrder();
     if ($('#profile-order').length){
       var clientOrders = new Vue({
         el: '#profile-order',
@@ -31,6 +31,15 @@ $(document).ready(function () {
           products: []
         },
         methods: {
+          getOrder(){
+            axios
+              .post('/ajax-get-order-page')
+              .then(response => {
+                console.log(response);
+                this.order = response.data;
+                console.log('заказ',this.order);
+              });
+          },
           isToday(someDate) {
               const today = new Date();
               return someDate.getDate() == today.getDate() &&
@@ -39,7 +48,7 @@ $(document).ready(function () {
           }
         },
         created() {
-          console.log('Order',this.order);
+          this.getOrder()
         }
       });
     }
