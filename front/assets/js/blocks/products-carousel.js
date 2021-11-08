0325af579d21edf2f6f7d04c427cf6bb7fc6a988
mvkorotych <korotychm@gmail.com>
2021-11-08 09:37:54 +0300

    test

--- front/assets/js/blocks/products-carousel.js
+++ front/assets/js/blocks/products-carousel.js
@@ -3,14 +3,32 @@ $(document).ready(function(){
     {
       infinite: true,
       slidesToShow: 4,
-      slidesToScroll: 4
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1267,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        }
+      ]
     }
   );
   $('.products-carousel--3').slick(
     {
       infinite: true,
       slidesToShow: 3,
-      slidesToScroll: 3
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        }
+      ]
     }
   );
 });
