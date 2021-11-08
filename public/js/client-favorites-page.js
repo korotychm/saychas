71cffdc9a9da4d55ec896bf923d4055e6f5098bd
Коротыч Михаил <m.korotich@saychas.office>
2021-11-08 07:16:11 +0300

    test

--- public/js/client-favorites-page.js
+++ public/js/client-favorites-page.js
@@ -17,3 +17,66 @@ $(function () {
     getClientProducts ("history");
 });
 
+
+$(document).ready(function(){
+
+  if ($('#user-favorites').length){
+
+    var categoryPage = new Vue({
+      el: '#user-favorites',
+      data: {
+        products: [],
+        length: 0,
+        sort: 0,
+        productsCount: 0,
+        productsCountUnit: 'товаров',
+        productsLimit: 0,
+        currentPage: 0
+      },
+      watch: {
+        sort() {
+          this.currentPage = 0;
+          this.getProducts();
+        }
+      },
+      methods: {
+        getProducts(){
+          axios
+            .post('/ajax-get-client-favorites',
+              Qs.stringify({
+                sort: this.sort,
+                page: this.currentPage
+              }))
+            .then(response => {
+              console.log(response);
+              this.products = response.data.products;
+              this.productsCount = response.data.count;
+              this.productsLimit = response.data.limit;
+              console.log('Favorites', this.products);
+              this.setUnit();
+              this.countPages();
+            });
+        },
+        loadPage(index) {
+          this.currentPage = index;
+          this.getProducts();
+        }
+      },
+      created() {
+          this.category_id = window.location.href.split("/").slice(-1)[0],
+          axios
+            .post('/ajax-get-client-favorites',
+              Qs.stringify({
+                categoryId : this.categoryId,
+                storeId: this.storeId
+              }))
+            .then(response => {
+              this.getProducts();
+              console.log('Favorites', this.products);
+            });
+      }
+    });
+
+  }
+
+});
