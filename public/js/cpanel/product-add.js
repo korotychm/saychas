4459dafef8204958694fba3e56926bf0265b8e4d
Коротыч Михаил <m.korotich@saychas.office>
2021-11-10 14:22:48 +0300

    test

--- public/js/cpanel/product-add.js
+++ public/js/cpanel/product-add.js
@@ -366,6 +366,7 @@ const ProductAdd = {
       let output = input;
       for (var i = 0; i < input.length; i++) {
             let c = input.charCodeAt(i);
+            console.log(input.charCodeAt(i));
             if ((c < 32 && c != 13) || c == 96 || (c > 255 && c < 1040) || (c > 1103 && c != 8470)) {
                 output = output.replace(input[i],'');
             }
