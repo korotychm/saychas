38adb47b22ef2de290ca14420907ab812c44d954
Вокуев Павел <vokuevpa1@saychas.office>
2021-11-12 12:47:25 +0300

    Test btn

--- public/js/cpanel/product-add.js
+++ public/js/cpanel/product-add.js
@@ -340,7 +340,7 @@ const ProductAdd = {
       originalProductWidth: null,
       originalProductHeigth: null,
       originalProductVat: null,
-      originalProductCharactristic: null
+      originalProductCharactristic: null,
     }
   },
   computed: {
