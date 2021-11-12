a10e639e6046dc9070709655a636c8fbd530304e
Вокуев Павел <vokuevpa1@saychas.office>
2021-11-12 15:06:39 +0300

    Test

--- public/js/cpanel/product-edit.js
+++ public/js/cpanel/product-edit.js
@@ -347,12 +347,12 @@ const ProductEdit = {
   },
   computed: {
     isChanged () {
-      if (JSON.stringify(this.product) !== this.originalData) {
-        console.log('зис продук',this.product)
-        console.log('оригинал продукт', JSON.parse(this.originalData))
-        console.log('оригинал дата сработала')
-        return true;
-      }
+      // if (JSON.stringify(this.product) !== this.originalData) {
+      //   console.log('зис продук',this.product)
+      //   console.log('оригинал продукт', JSON.parse(this.originalData))
+      //   console.log('оригинал дата сработала')
+      //   return true;
+      // }
       if (JSON.stringify(this.product.vendor_code) !== this.originalVendorCode) {
         console.log('оригинал вендор код')
         return true;
