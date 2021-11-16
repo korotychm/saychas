6df18b35c61b5ab162c2ea2bc19d15092addb2a2
Вокуев Павел <vokuevpa1@saychas.office>
2021-11-16 09:59:51 +0300

    test commit

--- public/js/cpanel/products.js
+++ public/js/cpanel/products.js
@@ -118,6 +118,13 @@ const Products = {
         requestUrl = '/control-panel/show-products-from-cache';
       }
       const headers = { 'X-Requested-With': 'XMLHttpRequest' }
+      console.log(          Qs.stringify({
+        page_no : this.page_no,
+        rows_per_page : this.rows_per_page,
+        filters: this.selectedFilters,
+        search: this.search,
+        use_cache: this.filtersCreated
+      }))
       axios
         .post(requestUrl,
           Qs.stringify({
