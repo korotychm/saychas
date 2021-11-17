2afbb52a89a206eb9ef3cce7bc98ccdbce6370c5
Вокуев Павел <vokuevpa1@saychas.office>
2021-11-17 13:35:49 +0300

    Test

--- public/js/cpanel/product-add-file.js
+++ public/js/cpanel/product-add-file.js
@@ -173,7 +173,7 @@ const ProductAddFile = {
         }),{headers})
           .then(response => {
             this.fileName = response.data.filename;
-            this.filePath = '/' + productsDocumentPath + this.fileName;
+            this.filePath = '/' + productsDocumentPath + 'bl_product_000000006.xls';
             this.filePathMoz = response.data.filename;
             console.log(productsDocumentPath)
             // this.filePathMoz = '/' + response.data.filename.replace(/^_/,'');
