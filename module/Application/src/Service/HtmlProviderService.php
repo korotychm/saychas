aaa6761ba8242103a1bf2e76d7ff64be621339b5
Коротыч Михаил <m.korotich@saychas.office>
2021-11-18 14:24:28 +0300

    test

--- module/Application/src/Service/HtmlProviderService.php
+++ module/Application/src/Service/HtmlProviderService.php
@@ -188,7 +188,7 @@ class HtmlProviderService
     private function payPossible($return)
     {
       $statusOk = Resource::ORDER_STATUS_CODE_REGISTRED;
-        if ($return['orderId'] != $statusOk['id'] ) {
+        if ($return['orderId'] !=1 ) {
             return false;
         }
 
