4a938428693fa530a894261bb6ad8dabcf3ea5d7
Коротыч Михаил <m.korotich@saychas.office>
2021-11-18 14:28:55 +0300

    test

--- public/js/client-order-page.js
+++ public/js/client-order-page.js
@@ -73,7 +73,7 @@ $(document).ready(function () {
               .then(response => {
                 console.log('tinkoff',response);
                 if (response.data.result){
-                  // переходим на страницу оплаты
+                  location.href(response.data.answer.PaymentURL);
                 } else {
                   showServicePopupWindow('Ошибка', 'Не удалось совершить оплату', "")
                 }
