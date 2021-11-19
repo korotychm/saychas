3b973636a0b82384896209273d67aaaa6a88c904
Вокуев Павел <vokuevpa1@saychas.office>
2021-11-19 16:17:55 +0300

    Refactor

--- public/js/cpanel/product-add-file.js
+++ public/js/cpanel/product-add-file.js
@@ -180,10 +180,6 @@ const ProductAddFile = {
           }
         }),{headers})
           .then(response => {
-            // if (response.data.filename.includes('1CMEDIA')) {
-            //   this.fileName = response.data.filename.replace('1CMEDIA/Providers', '')
-              // this.fileName = response.data.filename.replace('1CMEDIADEV/Providers', '')
-            // }
             this.fileName = response.data.filename;
             this.fileName = this.fileName.substr(this.fileName.indexOf('/P_') + 0)
             this.filePath = '/documents' + this.fileName;
