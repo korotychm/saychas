b84259321442b7b12005b4133716bd70b683ec88
Вокуев Павел <vokuevpa1@saychas.office>
2021-11-19 12:37:24 +0300

    test

--- public/js/cpanel/orders.js
+++ public/js/cpanel/orders.js
@@ -183,12 +183,12 @@ const Orders = {
         let i = 0;
         for (order of this.orders){
           if (+order.status_id == '01'){
-            let deadline = this.calulateTime(order.date,this.deadline_new,this.deadline_new_last);
+            let deadline = this.calulateTime(order.date / 1000,this.deadline_new,this.deadline_new_last);
             Vue.set(this.orders[i],'deadline',deadline);
             let blabla = new Date;
             this.currentTime = +blabla;
           } else if (+order.status_id == '02'){
-            let deadline = this.calulateTime(order.status_date,this.deadline_collect,this.deadline_collect_last);
+            let deadline = this.calulateTime(order.date / 1000,this.deadline_collect,this.deadline_collect_last);
             Vue.set(this.orders[i],'deadline',deadline);
             let blabla = new Date;
             this.currentTime = +blabla;
