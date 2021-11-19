74cbb3228b70428380e28bb844e4b58d0cafec4b
Вокуев Павел <vokuevpa1@saychas.office>
2021-11-19 12:21:28 +0300

    add console.log

--- public/js/cpanel/orders.js
+++ public/js/cpanel/orders.js
@@ -227,6 +227,8 @@ const Orders = {
       calulateTime(date,first,last){
         let current = (new Date().getTime()).toFixed();
         let deadline = +date + (first * 60);
+        console.log(current)
+        console.log(deadline)
         if (+current > deadline){
           let new_deadline = +date + ((first + last) * 60);
           if (+current > new_deadline){
