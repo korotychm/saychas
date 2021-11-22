2da43e7bfde51f037697a4717c953e2691266533
Коротыч Михаил <m.korotich@saychas.office>
2021-11-22 10:08:11 +0300

    test

--- public/js/basket.js
+++ public/js/basket.js
@@ -16,20 +16,15 @@ function setTimepointText(loadinfo = false) {
 // Изменение количества товара в корзине
 
 $(document).on('change','.cart__product-quantity input',function(e){
-  console.log('change');
   if ($(this).val() > +$(this).attr('max')){
     $(this).val(+$(this).attr('max'));
     $(this).parent().find('.cart__product-plus').addClass('disabled');
-  } else {
-    $(this).parent().find('.cart__product-plus').removeClass('disabled');
-  }
-  if ($(this).val() <= 0){
+  } else if ($(this).val() < 1){
     $(this).val(1);
     $(this).parent().find('.cart__product-minus').addClass('disabled');
   } else {
-    $(this).parent().find('.cart__product-minus').removeClass('disabled');
+    $(this).parent().find('button').removeClass('disabled');
   }
-
   let productId = $(this).data('product');
   $('.cart__checkbox input[data-product="' + productId + '"]').val($(this).val());
   calculateBasketItem(productId);
