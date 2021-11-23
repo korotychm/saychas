6f744f2757fa4965e7529dcfee5668c97f70c608
Вокуев Павел <vokuevpa1@saychas.office>
2021-11-23 16:04:39 +0300

    test

--- public/js/cpanel/price-list.js
+++ public/js/cpanel/price-list.js
@@ -137,8 +137,8 @@ const PriceList = {
       await axios
           .post(requestUrl,Qs.stringify({
             data: {
-              category_id: this.provider_id,
-              query_type: this.price
+              category_id: 'provider_id',
+              query_type: 'price'
             }
           }),{headers})
           .then(response => {
