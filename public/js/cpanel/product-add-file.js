f6c4c7c5d9076bf9a1f5b43b1b5af2620cd4cbbb
Вокуев Павел <vokuevpa1@saychas.office>
2021-11-23 11:34:27 +0300

    test

--- public/js/cpanel/product-add-file.js
+++ public/js/cpanel/product-add-file.js
@@ -93,8 +93,7 @@ const ProductAddFile = {
   },
   methods: {
     prefixer (item) {
-      console.log(item)
-      return '/documents/' + item
+      return '/documents/P_00005/product' + item
     },
     flatCategories() {
       let categoriesFlat = [];
