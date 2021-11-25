cc695b8d8cbe93531277b7302a9a772a269f981d
Вокуев Павел <vokuevpa1@saychas.office>
2021-11-25 16:43:32 +0300

    test

--- public/js/cpanel/common.js
+++ public/js/cpanel/common.js
@@ -187,7 +187,7 @@ function useKeyboardEvents () {
           e.preventDefault()
           $('.selected').siblings().prop('checked', true)
           console.log($('.selected').siblings())
-          this.$forceUpdate()
+          vm.$forceUpdate()
         }
         let $items =  $('.active').children('.custom-select__dropdown').children().children().children('span')
         let $selected = $items.filter('.selected').removeClass('selected')
