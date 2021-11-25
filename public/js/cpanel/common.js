3840ad34e789c5838d14867f40f0c766a0fcc0b1
Вокуев Павел <vokuevpa1@saychas.office>
2021-11-25 19:09:52 +0300

    test

--- public/js/cpanel/common.js
+++ public/js/cpanel/common.js
@@ -185,9 +185,8 @@ function useKeyboardEvents () {
   $(document).on( "keydown", function(e) {
         if (e.keyCode === 13) {
           e.preventDefault()
-          // $('.selected').siblings().prop('checked', true)
-          document.querySelector('input[type="radio"]').checked = true;
-          console.log($('.selected').siblings())
+          $('.selected').siblings().prop('checked', true)
+          console.log(document.querySelector('.selected').previousElementSibling)
         }
         let $items =  $('.active').children('.custom-select__dropdown').children().children().children('span')
         let $selected = $items.filter('.selected').removeClass('selected')
