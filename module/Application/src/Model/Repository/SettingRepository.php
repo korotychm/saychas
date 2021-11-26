27eccf5c768587ffa11f0e7edfbb711c6a2b50d4
Plusweb <plusw.ru@gmail.com>
2021-11-26 13:37:08 +0300

    fix

--- module/Application/src/Model/Repository/SettingRepository.php
+++ module/Application/src/Model/Repository/SettingRepository.php
@@ -57,25 +57,50 @@ class SettingRepository extends Repository implements SettingRepositoryInterface
         }
 
         if ((bool) $result['truncate']) {
-           // $this->db->query("truncate table {$this->tableName}")->execute();
-          return ['result' => false, 'description' => 'option truncate unavailble', 'statusCode' => 405];
-            
+            // $this->db->query("truncate table {$this->tableName}")->execute();
+            return ['result' => false, 'description' => 'option truncate unavailble', 'statusCode' => 405];
         }
 
 
-        
+
         $data = $result['data'];
-        
+
         $encodedData = Json::encode($data);
-        
+
         $setting = new Setting();
-        
+
         $setting->setId('main_menu');
         $setting->setValue($encodedData);
-        
+
         $this->persist($setting, ['id' => $setting->getId()]);
-        
+
         return ['result' => true, 'description' => '', 'statusCode' => 200];
+    }
+
+    public function replaceNew($content)
+    {
+        try {
+            $result = Json::decode($content, \Laminas\Json\Json::TYPE_ARRAY);
+        } catch (\Laminas\Json\Exception\RuntimeException $e) {
+            return ['result' => false, 'error_description' => $e->getMessage(), 'statusCode' => 400];
+        }
+
+        if (empty($data = $result['data']) or!is_array($data)) {
+            return ['result' => false, 'error_description' => 'data is empty', 'statusCode' => 400];
+        }
+
+        if ((bool) $result['truncate']) {
+            return ['result' => false, 'error_description' => 'option truncate unavailble', 'statusCode' => 405];
+        }
+
+        while (list($key, $value) = each($data)) {
+            $encodedData = Json::encode($value);
+            $setting = new Setting();
+            $setting->setId($key);
+            $setting->setValue($encodedData)->persist(['id' => $key]);
+        }
+
+        return ['result' => true, 'error_description' => '', 'statusCode' => 200];
     }
 
 }
