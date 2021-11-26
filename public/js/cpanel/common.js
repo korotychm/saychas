c7edcd8e8c9578948b6707c1459770b5a4fd941a
Вокуев Павел <vokuevpa1@saychas.office>
2021-11-26 15:38:39 +0300

    test

--- public/js/cpanel/common.js
+++ public/js/cpanel/common.js
@@ -195,7 +195,7 @@ Vue.directive('keyboard', {
       console.log(el)
       if (e.keyCode === 13) {
         e.preventDefault()
-        $('.selected').siblings().prop('checked', true)
+        el.checked = true;
         // document.querySelector('.selected').previousElementSibling.checked = true;
       }
       let $items =  $('.active').children('.custom-select__dropdown').children().children().children('span')
