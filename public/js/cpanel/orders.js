e1a8e505f81e430e1c443b45f25c448c9fd514d6
Коротыч Михаил <m.korotich@saychas.office>
2021-11-26 09:56:53 +0300

    test

--- public/js/cpanel/orders.js
+++ public/js/cpanel/orders.js
@@ -288,9 +288,10 @@ const Orders = {
                 location.reload();
               } else {
                 console.log('Response from update-requisition',response.data);
-                this.orders[index].requisition_sum_fact = response.data.data.requisition_sum_fact;
-                this.orders[index].status_id = response.data.data.status_id;
-                this.orders[index].status = response.data.data.status;
+                this.orders[index].requisition_sum_fact = response.data.data.dat.requisition_sum_fact;
+                this.orders[index].status_id = response.data.data.data.status_id;
+                this.orders[index].status = response.data.data.data.status;
+                this.orders[index].status_date = response.data.data.data.status_date;
               }
             })
             .catch(error => {
