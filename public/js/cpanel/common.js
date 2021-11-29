0e94e8e49c2cfe0db9f4e666222df0e47f0c44a9
Вокуев Павел <vokuevpa1@saychas.office>
2021-11-29 13:48:20 +0300

    test

--- public/js/cpanel/common.js
+++ public/js/cpanel/common.js
@@ -231,7 +231,7 @@ function useKeyboardEvents () {
 
           relativePos.top = childrenPos.top - parentPos.top
           console.log(relativePos)
-            $('.custom-select__dropdown-inner').scrollTop(relativePos.top)
+            $('.custom-select__dropdown-inner').scrollTop(relativePos.top - $('.custom-select__dropdown-inner').innerHeight())
         }
         if (e.keyCode === 	38) {
           e.preventDefault()
@@ -247,7 +247,7 @@ function useKeyboardEvents () {
 
           relativePos.top = childrenPos.top - parentPos.top
           console.log(relativePos)
-          $('.custom-select__dropdown-inner').scrollTop(relativePos.top)
+          $('.custom-select__dropdown-inner').scrollTop(relativePos.top - $('.custom-select__dropdown-inner').innerHeight())
         }
       }
   })
