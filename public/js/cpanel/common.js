0170a0e39cd7b491c2703485145e5ea47ecb3cf9
Вокуев Павел <vokuevpa1@saychas.office>
2021-11-29 13:23:31 +0300

    test

--- public/js/cpanel/common.js
+++ public/js/cpanel/common.js
@@ -225,7 +225,7 @@ function useKeyboardEvents () {
           $next.addClass('selected')
           // customSelectDropDownY -= 30
 
-          $('.custom-select__dropdown-inner').scrollTop($('.selected').offset().top -  $('.custom-select__dropdown-inner').clientHeight)
+          $('.custom-select__dropdown-inner').scrollTop($('.selected').offset().top -  $('.custom-select__dropdown-inner').innerHeight())
           console.log($next[0].getBoundingClientRect())
         }
         if (e.keyCode === 	38) {
@@ -238,7 +238,7 @@ function useKeyboardEvents () {
           $next.addClass('selected')
           // customSelectDropDownY -= 30
 
-          $('.custom-select__dropdown-inner').scrollTop($('.selected').offset().top - $('.custom-select__dropdown-inner').clientHeight)
+          $('.custom-select__dropdown-inner').scrollTop($('.selected').offset().top - $('.custom-select__dropdown-inner').innerHeight())
           console.log($next[0].getBoundingClientRect())
         }
       }
